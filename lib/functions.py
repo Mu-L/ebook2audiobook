f3231b06a64e35a8375d6711e9d5e5d05e73aa22
unknown <admin@radsl.net>
2025-08-20 05:03:12 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3865,7 +3865,6 @@ def web_interface(args, ctx):
                             window.init_elements = () =>{
                                 try{
                                     gr_root = (window.gradioApp && window.gradioApp()) || document;
-                                    gr_audiobook_player = gr_root.querySelector("#gr_audiobook_player audio");
                                     gr_tab_progress = gr_root.querySelector("#gr_tab_progress");
                                     gr_playback_time = gr_root.querySelector("#gr_playback_time input");
                                     gr_checkboxes = gr_root.querySelectorAll("input[type='checkbox']");
