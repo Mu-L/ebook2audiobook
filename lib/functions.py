8912326e66f91aec1cb1759b7d1332930c102b03
unknown <admin@radsl.net>
2025-08-20 07:26:11 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2387,7 +2387,7 @@ def web_interface(args, ctx):
                 height: 60px !important;
             }
             #gr_output_format_list {
-                height: 80px !important;
+                height: 120px !important;
             }
             #gr_ebook_mode span[data-testid="block-info"],
             #gr_voice_list span[data-testid="block-info"],
