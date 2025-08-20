bd733f01b2b3e3e4d66df75378fb01f4fe401109
unknown <admin@radsl.net>
2025-08-19 19:19:38 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2802,11 +2802,9 @@ def web_interface(args, ctx):
 
         def refresh_interface(id):
             session = context.get_session(id)
-            visible = True if len(audiobook_options) > 0 else False
             return (
                     gr.update(interactive=False), gr.update(value=None), update_gr_audiobook_list(id), 
                     gr.update(value=session['audiobook']), gr.update(visible=False), update_gr_voice_list(id),
-                    gr.update(visible=visible)
             )
 
         def change_gr_audiobook_list(selected, id):
@@ -3767,7 +3765,11 @@ def web_interface(args, ctx):
         ).then(
             fn=refresh_interface,
             inputs=[gr_session],
-            outputs=[gr_convert_btn, gr_ebook_file, gr_audiobook_list, gr_audiobook_player, gr_modal, gr_voice_list, gr_group_audiobook_list]
+            outputs=[gr_convert_btn, gr_ebook_file, gr_audiobook_list, gr_audiobook_player, gr_modal, gr_voice_list]
+        ).then(
+            fn=lambda: gr.update(visible=bool(audiobook_options)),
+            inputs=None,
+            outputs=[gr_group_audiobook_list],
         )
         gr_write_data.change(
             fn=None,
