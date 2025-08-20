920cabbee41bbf7d4419a7cebff5745a650c7e52
unknown <admin@radsl.net>
2025-08-20 16:31:07 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3029,7 +3029,6 @@ def web_interface(args, ctx):
             try:
                 if method is not None:
                     session = context.get_session(id)
-                    print(session)
                     if method == 'confirm_voice_del':
                         selected_name = Path(voice_path).stem
                         pattern = re.sub(r'\.wav$', '*.wav', voice_path)
