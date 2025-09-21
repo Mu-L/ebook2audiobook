d7c3f6d7ea0a80c4d69b15c62aefd1ff1ce689d9
unknown <admin@radsl.net>
2025-09-21 05:58:42 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3461,8 +3461,7 @@ def web_interface(args, ctx):
                                 session['event'] = progress_status
                                 msg = 'Select the blocks to convert:'
                                 print(msg)
-                                yield gr.update(msg), gr.update(show_modal(progress_status, msg),visible=True)
-                                return
+                                return gr.update(msg), gr.update(show_modal(progress_status, msg),visible=True)
                             else:
                                 show_alert({"type": "success", "msg": progress_status})
                                 reset_session(args['session'])
