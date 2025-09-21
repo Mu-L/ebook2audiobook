66c07e547bda702dc4b0387c9e98acaec8da1c34
unknown <admin@radsl.net>
2025-09-21 08:30:40 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3730,7 +3730,7 @@ def web_interface(args, ctx):
                 gr_xtts_temperature, gr_xtts_length_penalty, gr_xtts_num_beams, gr_xtts_repetition_penalty, gr_xtts_top_k, gr_xtts_top_p, gr_xtts_speed, gr_xtts_enable_text_splitting,
                 gr_bark_text_temp, gr_bark_waveform_temp, gr_output_split, gr_output_split_hours
             ],
-            outputs=[gr_progress, gr_modal]
+            outputs=[gr_tab_progress, gr_modal]
         )
         gr_write_data.change(
             fn=None,
