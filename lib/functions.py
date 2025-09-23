ecf9e2a1f410d4a29cfde003649087f7aeb0f6c5
unknown <admin@radsl.net>
2025-09-22 20:00:34 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2825,6 +2825,7 @@ def web_interface(args, ctx):
 
             return f"""
             <div style='margin:0; padding:0; font-size:12px; line-height:1.2; height:auto; display:flex; flex-wrap:wrap; align-items:center; gap:6px 12px;'>
+              <span style='display:inline-flex; white-space:nowrap; padding:0 10px'>TTS ENgine&nbsp;&nbsp;&nbsp;</span>
               <span style='display:inline-flex; white-space:nowrap; padding:0 10px'><b>GPU VRAM:</b> {color_box(rating["GPU VRAM"])}</span>
               <span style='display:inline-flex; white-space:nowrap; padding:0 10px'><b>CPU:</b> {yellow_stars(rating["CPU"])}</span>
               <span style='display:inline-flex; white-space:nowrap; padding:0 10px'><b>RAM:</b> {color_box(rating["RAM"])}</span>
