43a48c23b27ba4841e62caff06ffab5662575f05
unknown <admin@radsl.net>
2025-09-23 14:46:39 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2840,7 +2840,7 @@ def web_interface(args, ctx):
             rating = default_engine_settings[tts_engine]['rating']
 
             return f'''TTS Engine&nbsp;&nbsp;&nbsp;
-            <div style="margin:0; padding:0; font-size:12px; line-height:1.2; height:auto; display:inline-flex; flex-wrap:wrap; align-items:center; gap:6px 12px;">
+            <div style="margin:0; padding:0; font-size:12px; line-height:1.2; height:auto; display:inline-block; flex-wrap:wrap; align-items:center; gap:6px 12px; loat: right">
               <span style="display:inline-flex; white-space:nowrap; padding: 0 5px 0 0"><b>GPU VRAM:</b> {color_box(rating["GPU VRAM"])}</span>
               <span style="display:inline-flex; white-space:nowrap; padding: 0 5px 0 0"><b>CPU:</b> {yellow_stars(rating["CPU"])}</span>
               <span style="display:inline-flex; white-space:nowrap; padding: 0 5px 0 0"><b>RAM:</b> {color_box(rating["RAM"])}</span>
