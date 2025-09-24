a5d67cf5dfd60941d710e3228ff37a79ded38ffd
unknown <admin@radsl.net>
2025-09-24 07:33:44 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2873,7 +2873,7 @@ def web_interface(args, ctx):
             return f'''
                 <div style="display:flex; justify-content:space-between; align-items: center;">
                     <span class="gr-markdown-span">TTS Engine</span>
-                    <table style="display: inline-block; border-collapse: collapse; margin: 0; padding: 0; border: none; font-size: 12px;">
+                    <table style="display: inline-block; border-collapse: collapse; margin: 0; padding: 0; border: none; font-size: 12px; margin-top: 2px;">
                       <tr style="border: none">
                         <td style="padding: 0 5px 0 2.5px; margin: 0; border: none;"><b>GPU VRAM:</b> {color_box(rating["GPU VRAM"])}</td>
                         <td style="padding: 0 5px 0 2.5px; margin: 0; border: none;"><b>CPU:</b> {yellow_stars(rating["CPU"])}</td>
