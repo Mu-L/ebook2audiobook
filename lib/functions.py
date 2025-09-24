667ced593e9857f8e59ba12890e0b02fe3681c83
unknown <admin@radsl.net>
2025-09-23 20:06:42 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2626,6 +2626,7 @@ def web_interface(args, ctx):
                                         with gr_row_output_split_hours:
                                             gr_output_split_hours_markdown = gr.Markdown(elem_id='gr_output_split_hours_markdown', value='<div style="font-size: 12px; width:100%; text-align:center; vertical-align: middle; padding-top:15px; white-space: nowrap">Max Hours<br/>/Part</div>')
                                             gr_output_split_hours = gr.Dropdown(label='', elem_id='gr_output_split_hours', choices=options_output_split_hours, type='value', value=default_output_split_hours, interactive=True)
+                        with gr.Group(elem_id='gr_group_session', elem_classes=['custom-group']):
                             gr_session_markdown = gr.Markdown(elem_id='gr_session_markdown', elem_classes=['gr-markdown'], value='Session')
                             gr_session = gr.Textbox(label='', elem_id='gr_session', interactive=False)
                 gr_tab_xtts_params = gr.TabItem('XTTSv2 Fine Tuned Parameters', elem_id='gr_tab_xtts_params', elem_classes='gr-tab', visible=visible_gr_tab_xtts_params)           
