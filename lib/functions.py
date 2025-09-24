3c8ec409c28b6aaf013d2872dd95bf9560044b91
unknown <admin@radsl.net>
2025-09-24 07:35:36 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2871,17 +2871,25 @@ def web_interface(args, ctx):
             
             rating = default_engine_settings[tts_engine]['rating']
             return f'''
-                <div style="display:flex; justify-content:space-between; align-items: center;">
-                    <span class="gr-markdown-span">TTS Engine</span>
-                    <table style="display: inline-block; border-collapse: collapse; margin: 0; padding: 0; border: none; font-size: 12px; margin-top: 2px;">
-                      <tr style="border: none">
-                        <td style="padding: 0 5px 0 2.5px; margin: 0; border: none;"><b>GPU VRAM:</b> {color_box(rating["GPU VRAM"])}</td>
-                        <td style="padding: 0 5px 0 2.5px; margin: 0; border: none;"><b>CPU:</b> {yellow_stars(rating["CPU"])}</td>
-                        <td style="padding: 0 5px 0 2.5px; margin: 0; border: none;"><b>RAM:</b> {color_box(rating["RAM"])}</td>
-                        <td style="padding: 0 5px 0 2.5px; margin: 0; border: none;"><b>Realism:</b> {yellow_stars(rating["Realism"])}</td>
-                      </tr>
-                    </table>
-                </div>
+            <div style="display:flex; justify-content:space-between; align-items: center;">
+                <span class="gr-markdown-span">TTS Engine</span>
+                <table style="display: inline-block; border-collapse: collapse; margin: 0; padding: 0; border: none; font-size: 12px; line-height: 1.8;">
+                  <tr style="border: none; vertical-align: bottom;">
+                    <td style="padding: 0 5px 0 2.5px; margin: 0; border: none; vertical-align: bottom;">
+                      <b>GPU VRAM:</b> {color_box(rating["GPU VRAM"])}
+                    </td>
+                    <td style="padding: 0 5px 0 2.5px; margin: 0; border: none; vertical-align: bottom;">
+                      <b>CPU:</b> {yellow_stars(rating["CPU"])}
+                    </td>
+                    <td style="padding: 0 5px 0 2.5px; margin: 0; border: none; vertical-align: bottom;">
+                      <b>RAM:</b> {color_box(rating["RAM"])}
+                    </td>
+                    <td style="padding: 0 5px 0 2.5px; margin: 0; border: none; vertical-align: bottom;">
+                      <b>Realism:</b> {yellow_stars(rating["Realism"])}
+                    </td>
+                  </tr>
+                </table>
+            </div>
             '''
 
         def alert_exception(error):
