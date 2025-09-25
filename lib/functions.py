8ee80b0afddb43c071348544cb65955d9753ba6e
unknown <admin@radsl.net>
2025-09-24 19:39:06 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2656,7 +2656,7 @@ def web_interface(args, ctx):
                                 with gr.Row(elem_id='gr_row_output_format'):
                                     gr_row_output_split_hours = gr.Row(elem_id='gr_row_output_split_hours', visible=False)
                                     with gr_row_output_split_hours:
-                                        gr_output_split_hours_markdown = gr.Markdown(elem_id='gr_output_split_hours_markdown', value='<div style="background: var(--block-background-fill); font-size: 12px; width:100%; text-align:center; vertical-align: middle; padding-top:15px; padding-bottom:13px; white-space: nowrap">Max Hours<br/>/Part</div>')
+                                        gr_output_split_hours_markdown = gr.Markdown(elem_id='gr_output_split_hours_markdown', value='<div style="background: var(--block-background-fill); font-size: 12px; width:100%; text-align:center; vertical-align: middle; padding-top:14px; padding-bottom:13px; white-space: nowrap">Max Hours<br/>/Part</div>')
                                         gr_output_split_hours = gr.Dropdown(label='', elem_id='gr_output_split_hours', choices=options_output_split_hours, type='value', value=default_output_split_hours, interactive=True)
                             with gr.Group(elem_id='gr_group_session', elem_classes=['gr-group']):
                                 gr_session_markdown = gr.Markdown(elem_id='gr_session_markdown', elem_classes=['gr-markdown'], value='Session')
