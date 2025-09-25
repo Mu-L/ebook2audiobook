f70ecf0808225ec7cb64f5ea2084043f7e6859f6
unknown <admin@radsl.net>
2025-09-24 19:01:20 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2481,13 +2481,11 @@ def web_interface(args, ctx):
                 display: none !important;
             }
             #gr_row_ebook_mode { align-items: center !important; }
-            #gr_chapters_control {
-                align-self: center !important; 
-                overflow: visible !important;
-            }
             #gr_chapters_control > label {
                 display: flex !important;
                 align-items: center !important;
+                align-self: center !important; 
+                overflow: visible !important;
                 gap: 6px;
                 line-height: 1.2 !important;
             }
