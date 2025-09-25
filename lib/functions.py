84df443c50236f3637f7506cc5778263f534ccce
unknown <admin@radsl.net>
2025-09-24 19:56:40 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3385,7 +3385,7 @@ def web_interface(args, ctx):
                 return (
                        gr.update(value=show_rating(session['tts_engine'])), 
                        gr.update(visible=visible_gr_tab_xtts_params), gr.update(visible=False), gr.update(visible=visible_custom_model), update_gr_fine_tuned_list(id),
-                       gr.update(label=f"*Upload {session['tts_engine']} Model (Should be a ZIP file with {', '.join(models[session['tts_engine']][default_fine_tuned]['files'])})"),
+                       gr.update(label=f"*Upload {session['tts_engine']} Model (.zip {', '.join(models[session['tts_engine']][default_fine_tuned]['files'])})"),
                        gr.update(label=f"My {session['tts_engine']} custom models")
                 )
             else:
