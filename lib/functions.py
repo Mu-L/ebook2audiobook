bc08c2e600b7cb1f5816df3d765a6db6ad543d1f
unknown <admin@radsl.net>
2025-09-24 19:04:36 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2484,7 +2484,7 @@ def web_interface(args, ctx):
             #gr_chapters_control {
                 align-self: center !important; 
                 overflow: visible !important;
-                padding: 30px 0 30px 0;
+                padding: 20px 0 20px 0;
             }
             #gr_tts_rating {
                 overflow: hidden !important;
