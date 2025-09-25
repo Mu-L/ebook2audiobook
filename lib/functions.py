e2d4fadd6aae7ebb20880a51321dcad98827d9e5
unknown <admin@radsl.net>
2025-09-25 05:44:31 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2349,7 +2349,8 @@ def web_interface(args, ctx):
                 margin: 0 !important;
             }
             .icon-btn {
-                margin-bottom: 30px !important;
+                padding-bottom: 5px !important;
+                margin-bottom: 5px !important;
                 font-size: 30px !important;
             }
             .small-btn{
