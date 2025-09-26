c1c9e6475815dfdd1d348a97da70f24d272146ab
unknown <admin@radsl.net>
2025-09-25 19:16:19 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2313,7 +2313,7 @@ def web_interface(args, ctx):
             button div.wrap::after {
                 content: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%23000' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4'/><polyline points='17 8 12 3 7 8'/><line x1='12' y1='3' x2='12' y2='15'/></svg>") !important;
                 display: inline-block !important;
-                color: var(--neutral-100) !important;
+                color: white !important;
                 width: 24px !important;
                 height: 24px !important;
                 vertical-align: middle !important;
