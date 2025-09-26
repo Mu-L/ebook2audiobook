02a42abe8a3b8391a1b66508e103203ad53445ae
unknown <admin@radsl.net>
2025-09-25 18:49:44 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2307,7 +2307,18 @@ def web_interface(args, ctx):
                 scrollbar-width: thin !important;
                 scrollbar-color: #c09340 none !important;
             }
-
+            button div.wrap span {
+                display: none !important;
+            }
+            button div.wrap::after {
+                content: url('<svg xmlns="http://www.w3.org/2000/svg" width="90%" height="90%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-upload"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line></svg>') !important;
+                display: inline-block !important;
+                width: 24px !important;
+                height: 24px !important;
+                font-size: 14px !important;
+                vertical-align: middle !important;
+                padding: 8px !important;
+            }
             .svelte-1xyfx7i.center.boundedheight.flex{
                 height: 120px !important;
             }
