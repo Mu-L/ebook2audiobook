91cab69471e271b057f08a00bc914d7420b6520e
unknown <admin@radsl.net>
2025-09-25 19:30:28 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2311,21 +2311,11 @@ def web_interface(args, ctx):
                 display: none !important;
             }
             button div.wrap::after {
-                content: url("data:image/svg+xml;utf8,
-                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'
-                    fill='none'
-                    stroke='%231E90FF'
-                    stroke-width='2'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'>
-                    <path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4'/>
-                    <polyline points='17 8 12 3 7 8'/>
-                    <line x1='12' y1='3' x2='12' y2='15'/>
-                    </svg>");
-                width: 24px;
-                height: 24px;
-                display: inline-block;
-                vertical-align: middle;
+                content: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%231E90FF' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4'/><polyline points='17 8 12 3 7 8'/><line x1='12' y1='3' x2='12' y2='15'/></svg>") !important;
+                width: 24px !important;
+                height: 24px !important;
+                display: inline-block !important;
+                vertical-align: middle !important;
             }
             .svelte-1xyfx7i.center.boundedheight.flex{
                 height: 120px !important;
