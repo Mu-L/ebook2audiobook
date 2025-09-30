52387149c944a540e7b45d98d130e4e48d1b6bb3
unknown <admin@radsl.net>
2025-09-30 06:08:21 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2835,7 +2835,7 @@ def web_interface(args, ctx):
                 gr_audiobook_vtt = gr.Textbox(elem_id='gr_audiobook_vtt', label='', interactive=False)
                 gr_playback_time = gr.Number(elem_id="gr_playback_time", label='', interactive=False, value=0.0)
                 gr_audiobook_sentence = gr.Textbox(elem_id='gr_audiobook_sentence', label='', value='...', interactive=False, visible=True, lines=3, max_lines=3)
-                gr_audiobook_player = gr.Audio(elem_id='gr_audiobook_player', label='',type='filepath', value='Empty', autoplay=False, waveform_options=gr.WaveformOptions(show_recording_waveform=False), show_download_button=False, show_share_button=False, container=True, interactive=False, visible=True)
+                gr_audiobook_player = gr.Audio(elem_id='gr_audiobook_player', label='',type='filepath', value=None, autoplay=False, waveform_options=gr.WaveformOptions(show_recording_waveform=False), show_download_button=False, show_share_button=False, container=True, interactive=False, visible=True)
                 with gr.Row(elem_id='gr_row_audiobook_list'):
                     gr_audiobook_download_btn = gr.Button(elem_id='gr_audiobook_download_btn', value='↧', elem_classes=['small-btn'], variant='secondary', interactive=True, visible=True, scale=0, min_width=60)
                     gr_audiobook_list = gr.Dropdown(elem_id='gr_audiobook_list', label='', choices=audiobook_options, type='value', interactive=True, visible=True, scale=2)
@@ -3930,13 +3930,13 @@ def web_interface(args, ctx):
             show_progress="minimal",
         )
         gr_audiobook_list.change(
-            fn=change_gr_audiobook_list,
-            inputs=[gr_audiobook_list, gr_session],
-            outputs=[gr_audiobook_player]
-        ).then(
             fn=lambda audiobook: load_vtt_data(audiobook),
             inputs=[gr_audiobook_list],
             outputs=[gr_audiobook_vtt]
+        ).then(
+            fn=change_gr_audiobook_list,
+            inputs=[gr_audiobook_list, gr_session],
+            outputs=[gr_audiobook_player]
         ).then(
             fn=None,
             inputs=[gr_audiobook_vtt],
