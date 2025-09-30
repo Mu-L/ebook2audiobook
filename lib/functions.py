a8f977b7571bc8cf3f181402ed1de1aab02bc1dd
unknown <admin@radsl.net>
2025-09-30 05:10:44 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3701,7 +3701,7 @@ def web_interface(args, ctx):
                     else None
                 )
                 session['playback_time'] = 0
-                audiobook_options.insert(0, [])
+                audiobook_options.insert(0, None)
                 if len(audiobook_options) > 0:
                     if session['audiobook'] is not None:
                         return gr.update(choices=audiobook_options, value=session['audiobook'])
