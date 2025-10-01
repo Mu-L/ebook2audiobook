c84c515e0b4ec583f70712c7754fb0bfc5235bd2
unknown <admin@radsl.net>
2025-10-01 05:29:54 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2839,7 +2839,7 @@ def web_interface(args, ctx):
             with gr.Group(elem_id='gr_group_progress', elem_classes=['gr-group-sides-padded']):
                 gr_progress_markdown = gr.Markdown(elem_id='gr_progress_markdown', elem_classes=['gr-markdown'], value='Progress')
                 gr_tab_progress = gr.Textbox(elem_id='gr_tab_progress', label='', interactive=False, visible=True)
-            gr_group_audiobook_list = gr.Group(elem_id='gr_group_audiobook_list', elem_classes=['gr-group-sides-padded'], visible='hidden')
+            gr_group_audiobook_list = gr.Group(elem_id='gr_group_audiobook_list', elem_classes=['gr-group-sides-padded'], visible=False)
             with gr_group_audiobook_list:
                 gr_audiobook_markdown = gr.Markdown(elem_id='gr_audiobook_markdown', elem_classes=['gr-markdown'], value='Audiobook')
                 gr_audiobook_vtt = gr.Textbox(elem_id='gr_audiobook_vtt', label='', interactive=False)
@@ -2850,7 +2850,7 @@ def web_interface(args, ctx):
                     gr_audiobook_download_btn = gr.Button(elem_id='gr_audiobook_download_btn', value='↧', elem_classes=['small-btn'], variant='secondary', interactive=True, scale=0, min_width=60)
                     gr_audiobook_list = gr.Dropdown(elem_id='gr_audiobook_list', label='', choices=audiobook_options, type='value', interactive=True, scale=2)
                     gr_audiobook_del_btn = gr.Button(elem_id='gr_audiobook_del_btn', value='🗑', elem_classes=['small-btn-red'], variant='secondary', interactive=True, scale=0, min_width=60)
-                gr_audiobook_files = gr.Files(label="Downloads", elem_id='gr_audiobook_files', visible='hidden')
+                gr_audiobook_files = gr.Files(label="Downloads", elem_id='gr_audiobook_files', visible=False)
                 gr_audiobook_files_toggled = gr.State(False)
             with gr.Group(elem_id='gr_convert_btn', elem_classes=['gr-group-convert-btn']):
                 gr_convert_btn = gr.Button(elem_id='gr_convert_btn', value='📚', elem_classes='gr-convert-btn', variant='primary', interactive=False)
@@ -3043,17 +3043,17 @@ def web_interface(args, ctx):
                 if audiobook is not None: 
                     vtt = Path(audiobook).with_suffix('.vtt')
                     if not os.path.exists(audiobook) or not os.path.exists(vtt):
-                        return gr.update(visible=True if len(audiobook_options) > 0 else 'hidden'), gr.update(value=None), gr.update(value=None)
+                        return gr.update(visible=True if len(audiobook_options) > 0 else False), gr.update(value=None), gr.update(value=None)
                     session['playback_time'] = 0
                     audio_info = mediainfo(audiobook)
                     session['duration'] = float(audio_info['duration'])
                     with open(vtt, "r", encoding="utf-8-sig", errors="replace") as f:
                         vtt_content = f.read()
-                    return gr.update(visible=True if len(audiobook_options) > 0 else 'hidden'), gr.update(value=audiobook), gr.update(value=vtt_content)
+                    return gr.update(visible=True if len(audiobook_options) > 0 else False), gr.update(value=audiobook), gr.update(value=vtt_content)
             except Exception as e:
                 error = f'change_gr_audiobook_list(): {e}'
                 alert_exception(error)
-            return gr.update(visible=True if len(audiobook_options) > 0 else 'hidden'), gr.update(value=None), gr.update(value=None)
+            return gr.update(visible=True if len(audiobook_options) > 0 else False), gr.update(value=None), gr.update(value=None)
         
         def update_gr_glass_mask(str=gr_glass_mask_msg, attr=['gr-glass-mask']):
             return gr.update(value=str, elem_id='gr_glass_mask', elem_classes=attr)
