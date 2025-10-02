7ccc49eb32b040ea17e9dd8f44d57e556f293d31
unknown <admin@radsl.net>
2025-10-01 20:16:28 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -4466,6 +4466,7 @@ def web_interface(args, ctx):
                         window.playback_time = 0;
                         const stored_session = window.localStorage.getItem("data");
                         const stored_volume = localStorage.getItem("volume");
+                        console.log("stored_volume", stored_volume);
                         if(stored_session){
                             const parsed = JSON.parse(stored_session);
                             parsed.tab_id = "tab-" + performance.now().toString(36) + "-" + Math.random().toString(36).substring(2, 10);
