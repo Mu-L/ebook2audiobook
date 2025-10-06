c057f1864f0fcc4fbdb5c4e76ee664a6b7835070
unknown <admin@radsl.net>
2025-10-06 05:11:41 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2844,7 +2844,7 @@ def web_interface(args, ctx):
                             info='Higher values lead to more creative, unpredictable outputs. Lower values make it more conservative.'
                         )
             with gr.Group(elem_id='gr_group_progress', elem_classes=['gr-group-sides-padded']):
-                gr_progress_markdown = gr.Markdown(elem_id='gr_progress_markdown', elem_classes=['gr-markdown'], value='Progress')
+                gr_progress_markdown = gr.Markdown(elem_id='gr_progress_markdown', elem_classes=['gr-markdown'], value='Status')
                 gr_tab_progress = gr.Textbox(elem_id='gr_tab_progress', label='', interactive=False, visible=True)
             gr_group_audiobook_list = gr.Group(elem_id='gr_group_audiobook_list', elem_classes=['gr-group-sides-padded'], visible=True)
             with gr_group_audiobook_list:
