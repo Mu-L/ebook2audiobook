57688208fa909db1e48c9fdd6ddd56fea611e2f5
unknown <admin@radsl.net>
2025-10-07 08:23:58 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3036,11 +3036,11 @@ def web_interface(args, ctx):
         def refresh_interface(id):
             session = context.get_session(id)
             if session['event'] == 'confirm_blocks':
-                return gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update()
+                return gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update()
             else:
                 return (
                     gr.update(interactive=False), gr.update(value=None), update_gr_audiobook_list(id), 
-                    gr.update(value=session['audiobook']), gr.update(visible=False), update_gr_voice_list(id),
+                    gr.update(value=session['audiobook']), gr.update(visible=False), update_gr_voice_list(id), gr.update(value='...')
                 )
 
         def change_gr_audiobook_list(selected, id):
@@ -4109,7 +4109,7 @@ def web_interface(args, ctx):
         ).then(
             fn=refresh_interface,
             inputs=[gr_session],
-            outputs=[gr_convert_btn, gr_ebook_file, gr_audiobook_list, gr_audiobook_player, gr_modal, gr_voice_list]
+            outputs=[gr_convert_btn, gr_ebook_file, gr_audiobook_list, gr_audiobook_player, gr_modal, gr_voice_list gr_progress]
         )
         gr_write_data.change(
             fn=None,
@@ -4181,7 +4181,7 @@ def web_interface(args, ctx):
         ).then(
             fn=refresh_interface,
             inputs=[gr_session],
-            outputs=[gr_convert_btn, gr_ebook_file, gr_audiobook_list, gr_audiobook_player, gr_modal, gr_voice_list]
+            outputs=[gr_convert_btn, gr_ebook_file, gr_audiobook_list, gr_audiobook_player, gr_modal, gr_voice_list, gr_progress]
         )
         gr_confirm_blocks_no_btn.click(
             fn=lambda session: confirm_blocks("no", session),
