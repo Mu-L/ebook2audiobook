a01f6cbbe618f480576747202d2ef741ecd25d43
unknown <admin@radsl.net>
2025-10-11 20:11:56 -0700

    ...

--- lib/classes/progress_handler.py
+++ lib/classes/progress_handler.py
@@ -6,42 +6,42 @@ class ProgressHandler:
         self.progress_bar = gr.Progress(track_tqdm=False) if session.get("is_gui_process") else None
 
     def on_start(self):
-        print("Export started")
-        if self.progress_bar:
-            try:
-                self.progress_bar(0, desc="Starting export…")
-            except Exception:
-                pass
+        try:
+            print("Final export started")
+            if session.get("is_gui_process"):
+                self.progress_bar(0, desc="Starting Final export…")
+        except Exception:
+            pass
 
     def on_progress(self, percent):
-        sys.stdout.write(f"\rExport progress: {percent:.1f}%")
-        sys.stdout.flush()
-        if self.progress_bar:
-            try:
-                self.progress_bar(percent / 100, desc=f"Encoding {percent:.1f}%")
-            except Exception:
-                pass
+        try:
+            sys.stdout.write(f"\Final export progress: {percent:.1f}%")
+            sys.stdout.flush()
+            if session.get("is_gui_process"):
+                self.progress_bar(percent / 100, desc=f"Final export")
+        except Exception:
+            pass
 
     def on_complete(self, *_):
-        print("\nExport completed successfully")
-        if self.progress_bar:
-            try:
+        try:
+            print("\Final export  completed successfully")
+            if session.get("is_gui_process"):
                 self.progress_bar(1.0, desc="Export completed")
-            except Exception:
-                pass
+        except Exception:
+            pass
 
     def on_error(self, err):
-        print(f"\nExport failed: {err}")
-        if self.progress_bar:
-            try:
+        try:
+            print(f"\Final export failed: {err}")
+            if session.get("is_gui_process"):
                 self.progress_bar(0.0, desc="Export failed")
-            except Exception:
-                pass
+        except Exception:
+            pass
 
     def on_cancel(self):
-        print("\nExport cancelled")
-        if self.progress_bar:
-            try:
+        try:
+            print("\Final export cancelled")
+            if session.get("is_gui_process"):
                 self.progress_bar(0.0, desc="Cancelled")
-            except Exception:
-                pass
+        except Exception:
+            pass
