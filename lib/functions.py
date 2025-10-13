4381e88e6c2db386aca0587cce32640bd7783600
unknown <admin@radsl.net>
2025-10-12 18:17:21 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -958,7 +958,7 @@ def get_sentences(text, lang, tts_engine):
                                 sentences.append(text_part)
                             text_part = w
                     if text_part:
-                        cleaned = re.sub(r'[^\p{L}\p{N} ]+', '', text_part).strip()
+                        cleaned = re.sub(r'[^\p{L}\p{N} \-—]+', '', text_part).strip()
                         if not any(ch.isalnum() for ch in cleaned):
                             continue
                         sentences.append(text_part)
