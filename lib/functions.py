183b92de6b7d41ed25e4137c5962ecd39b626c77
unknown <admin@radsl.net>
2025-10-12 17:45:04 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -10,8 +10,8 @@ import torch
 _original_load = torch.load
 
 def patched_torch_load(*args, **kwargs):
-	kwargs.setdefault("weights_only", False)
-	return _original_load(*args, **kwargs)
+    kwargs.setdefault("weights_only", False)
+    return _original_load(*args, **kwargs)
 
 torch.load = patched_torch_load
 
