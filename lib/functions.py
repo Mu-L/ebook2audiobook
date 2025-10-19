b91b9fb03c00eb943c62c89f6d2377603ebd9ded
unknown <admin@radsl.net>
2025-10-19 13:56:00 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2168,8 +2168,6 @@ def restore_session_from_data(data:dict, session:dict)->None:
                 else:
                     if value is None and session[key] is not None:
                         continue
-                    if key == 'ebook_list':
-                        value = json.loads(value)
                     session[key] = value
     except Exception as e:
         DependencyError(e)
@@ -2990,8 +2988,8 @@ def web_interface(args:dict, ctx:SessionContext)->None:
                     return outputs
                 ebook_data = None
                 file_count = session['ebook_mode']
-                if isinstance(session['ebook_list'], list) and file_count == 'directory':
-                    ebook_data = list(session['ebook_list'])
+                if session['ebook_list'] is not None and file_count == 'directory':
+                    ebook_data = session['ebook_list']
                 elif isinstance(session['ebook'], str) and file_count == 'single':
                     ebook_data = str(session['ebook'])
                 else:
@@ -3027,15 +3025,6 @@ def web_interface(args:dict, ctx:SessionContext)->None:
                     gr.update(interactive=False), gr.update(value=None), update_gr_audiobook_list(id), 
                     gr.update(value=session['audiobook']), gr.update(visible=False), update_gr_voice_list(id), gr.update(value='')
                 )
-                
-        def handle_directory_change(data:list, id:str)->str:
-            session = context.get_session(id)
-            ebook_list = []
-            if isinstance(data, list):
-                for f in data:
-                    path = f.get("path") if isinstance(f, dict) else str(f)
-                    ebook_list.append(path)
-            return json.dumps(ebook_list, indent=2)
 
         def change_gr_audiobook_list(selected:any, id:str)->gr.update:
             try:
@@ -3107,7 +3096,11 @@ def web_interface(args:dict, ctx:SessionContext)->None:
                         yield gr.update(value=show_gr_modal('wait', msg), visible=True)
                         return
                 if isinstance(data, list):
-                    session['ebook_list'] = handle_directory_change(data, id)
+                    ebook_list = []
+                    for f in data:
+                        path = f.get("path") if isinstance(f, dict) else str(f)
+                        ebook_list.append(path)
+                    session['ebook_list'] = json.dumps({"ebook_list": ebook_list}, indent=2)
                 else:
                     session['ebook'] = data 
                 session['cancellation_requested'] = False
