07beb4e7a132b573fd901cfc97088d310f72f853
unknown <admin@radsl.net>
2025-10-23 20:30:37 -0700

    ...

--- lib/classes/voice_extractor.py
+++ lib/classes/voice_extractor.py
@@ -201,6 +201,7 @@ class VoiceExtractor:
                 filepath
             ]
             result = subprocess.run(cmd, capture_output=True, text=True)
+            print(json.loads(result.stdout))
             try:
                 duration = json.loads(result.stdout)['format']['duration']
                 return float(duration)
