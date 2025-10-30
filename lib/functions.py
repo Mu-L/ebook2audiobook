f18c98b6fa635ebb0b2c09c76f6634371a3195e9
unknown <admin@radsl.net>
2025-10-29 18:14:05 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -556,8 +556,7 @@ YOU CAN IMPROVE IT OR ASK TO A TRAINING MODEL EXPERT.
         stanza_nlp = False
         if session['language'] in year_to_decades_languages:
             try:
-                stanza_model = os.path.join(os.getenv("STANZA_RESOURCES_DIR"), session['language_iso1'], 'default.zip')
-                stanza.download(session['language_iso1'], download_method=None)
+                stanza.download(session['language_iso1'], model_dir=os.getenv('STANZA_RESOURCES_DIR'))
                 stanza_nlp = stanza.Pipeline(session['language_iso1'], processors='tokenize,ner,mwt', use_gpu=False)
             except (ConnectionError, TimeoutError) as e:
                 error = f'Stanza model download connection error: {e}. Retry later'
