c8da44e807f93468b2ebeffc1a8e7c822f736af1
unknown <admin@radsl.net>
2025-11-03 09:11:27 -0800

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -4297,7 +4297,7 @@ def build_interface(args:dict)->gr.Blocks:
                 inputs=[gr_session],
                 outputs=[gr_ebook_mode, gr_chapters_preview, gr_language, gr_voice_file, gr_voice_list, gr_device, gr_tts_engine_list, gr_fine_tuned_list, gr_custom_model_file, gr_custom_model_list]
             )
-            //////////
+            ############
             app.load(
                 fn=None,
                 js=r'''
