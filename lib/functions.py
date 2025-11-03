93c0ff74fb07df8e2baf734d8c1d39b78e34dfc4
unknown <admin@radsl.net>
2025-11-03 03:59:51 -0800

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -4789,7 +4789,7 @@ def build_interface(args:dict, ctx:SessionContext)->gr.Blocks:
             msg = f'IPs available for connection:\n{all_ips}\nNote: 0.0.0.0 is not the IP to connect. Instead use an IP above to connect and port {interface_port}'
             show_alert({"type": "info", "msg": msg})
             os.environ['no_proxy'] = ' ,'.join(all_ips)
-            return app
+            app.queue(default_concurrency_limit=interface_concurrency_limit).launch(debug=bool(int(os.environ.get('GRADIO_DEBUG', '0'))),show_error=debug_mode, favicon_path='./favicon.ico', server_name=interface_host, server_port=interface_port, share= args['share'], max_file_size=max_upload_size)
     except OSError as e:
         error = f'Connection error: {e}'
         alert_exception(error, None)
@@ -4802,4 +4802,3 @@ def build_interface(args:dict, ctx:SessionContext)->gr.Blocks:
     except Exception as e:
         error = f'An unexpected error occurred: {e}'
         alert_exception(error, None)
-    return
