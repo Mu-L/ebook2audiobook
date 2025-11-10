e77a5bd2710c283f10e7c3fbd09092d1a57be4db
unknown <admin@radsl.net>
2025-11-10 14:06:22 -0800

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -558,13 +558,15 @@ YOU CAN IMPROVE IT OR ASK TO A TRAINING MODEL EXPERT.
         stanza_nlp = False
         if session['language'] in year_to_decades_languages:
             try:
-                stanza_nlp = loaded_tts.get(f"stanza-{session['language_iso1']}", False)
+                stanza_model = f"stanza-{session['language_iso1']}"
+                stanza_nlp = loaded_tts.get(stanza_model, False)
                 if not stanza_nlp:
                     use_gpu = True if (session['device'] == devices['CUDA']['proc'] and devices['CUDA']['found']) or (session['device'] == devices['ROCM']['proc'] and devices['ROCM']['found']) or (session['device'] == devices['XPU']['proc'] and devices['XPU']['found'])else False
                     stanza.download(session['language_iso1'], model_dir=os.getenv('STANZA_RESOURCES_DIR'))
                     stanza_nlp = stanza.Pipeline(session['language_iso1'], processors='tokenize,ner,mwt', use_gpu=use_gpu, download_method="reuse_resources")
                     if stanza_nlp:
-                        session['stanza_cache'] = stanza_nlp
+                        session['stanza_cache'] = stanza_model
+                        loaded_tts[stanza_model] = stanza_nlp
             except (ConnectionError, TimeoutError) as e:
                 error = f'Stanza model download connection error: {e}. Retry later'
                 return error, None
