5c660f5fef32cd54e992947fa4e2ee0d6b04cc6b
unknown <admin@radsl.net>
2025-11-12 08:21:52 -0800

    ...

--- ebook2audiobook.sh
+++ ebook2audiobook.sh
@@ -12,7 +12,7 @@ PYTHON_VERSION=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.v
 MIN_PYTHON_VERSION="3.10"
 MAX_PYTHON_VERSION="3.13"
 
-esport APP_ROOT="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")" && pwd)"
+export APP_ROOT="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")" && pwd)"
 export PYTHONUTF8="1"
 export PYTHONIOENCODING="utf-8"
 
