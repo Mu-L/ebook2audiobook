390e79d6395324decfafa08e642d5a20a6d03942
unknown <admin@radsl.net>
2025-11-18 05:53:42 -0800

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -4157,7 +4157,7 @@ def build_interface(args:dict)->gr.Blocks:
             gr_custom_model_file.upload(
                 fn=change_gr_custom_model_file,
                 inputs=[gr_custom_model_file, gr_tts_engine_list, gr_session],
-                outputs=[gr_custom_model_file, gr_custom_model_list, gr_row_voice_player],
+                outputs=[gr_custom_model_file, gr_custom_model_list, gr_group_voice_file],
                 show_progress_on=[gr_custom_model_list]
             )
             gr_custom_model_list.change(
