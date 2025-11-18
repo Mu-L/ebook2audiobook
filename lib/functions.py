a77b845412d0d99835fbc007f36e3b48905b3500
unknown <admin@radsl.net>
2025-11-17 19:23:10 -0800

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2630,6 +2630,7 @@ def build_interface(args:dict)->gr.Blocks:
                     font-size: 12px !important;
                     border: none !important;
                     overflow-y: hidden !important;
+                    line-height: 0!important;
                 }
                 .gr-markdown p {
                     margin-top: 8px !important;
