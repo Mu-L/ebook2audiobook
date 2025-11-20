9fb289bea7d76086e66c2b24839235b9181287e0
unknown <admin@radsl.net>
2025-11-19 19:39:56 -0800

    ...

--- lib/models.py
+++ lib/models.py
@@ -41,7 +41,7 @@ default_engine_settings = {
         "temperature": 0.75,
         "length_penalty": 1.0,
         "num_beams": 1,
-        "repetition_penalty": 3.0,
+        "repetition_penalty": 2.0,
         "top_k": 50,
         "top_p": 0.85,
         "speed": 1.0,
