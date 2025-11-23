c25b736c038ac9de486ee08d51a61ba0f1a27e88
unknown <admin@radsl.net>
2025-11-22 19:20:37 -0800

    ...

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sysconfig
 import shutil
 
-from lib.conf import default_device, components_dir, devices
+from lib.conf import default_device, components_dir, devices, default_output_format
 
 ########## sitecustomize.py
 site_packages_path = sysconfig.get_paths()['purelib']
