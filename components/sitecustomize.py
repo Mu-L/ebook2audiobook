5586bf6dd24a0bb56e9783f8b19377af49f849b6
unknown <admin@radsl.net>
2025-11-27 16:54:12 -0800

    ...

--- components/sitecustomize.py
+++ components/sitecustomize.py
@@ -7,20 +7,24 @@ Use for lightweight, idempotent environment patches.
 import sys
 import importlib
 
+debug = True
+
+def warn(msg:str)->None:
+    if debug:
+        print(msg)
+
+def wrapped_check_torch_load_is_safe(*args, **kwargs):
+    warn("[sitecustomize] Hook: transformers check called")
+    pass
+
 if not getattr(sys, "_sitecustomize_loaded", False):
     sys._sitecustomize_loaded = True
     try:
         iu = importlib.import_module("transformers.utils.import_utils")
         if not hasattr(iu, "_patch_applied"):
-            #original_check = iu.check_torch_load_is_safe
-
-            def wrapped_check_torch_load_is_safe(*args, **kwargs):
-                #print("[sitecustomize] Hook: transformers check called")
-                pass
-
             iu.check_torch_load_is_safe = wrapped_check_torch_load_is_safe
             iu._patch_applied = True
-            #print("[sitecustomize] transformers hook installed")
+            warn("[sitecustomize] transformers hook installed")
     except ModuleNotFoundError:
-        #print("[sitecustomize] transformers not available; skipping patch")
+        warn("[sitecustomize] transformers not available; skipping patch")
         pass
