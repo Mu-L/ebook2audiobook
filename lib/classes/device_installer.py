75ce90aa7e19cc908fbdd8d7344226fa9cbb2148
unknown <admin@radsl.net>
2025-11-30 15:42:21 -0800

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -441,18 +441,20 @@ class DeviceInstaller():
             if device_info:
                 torch_version = self.check_torch_version
                 if torch_version:
-                    torch_version_base = Version(torch_version).base_version
-                    print(f"{device_info['name']} hardware found! Installing the right torch library...")
                     if device_info['tag'] not in ['cpu', 'unknown', 'unsupported']:
                         m = re.search(r'\+(.+)$', torch_version)
                         current_tag = m.group(1) if m else None
+                        print(f'current tag: {current_tag}')
                         if current_tag is not None:
                             non_standard_tag = re.fullmatch(r'[0-9a-f]{7,40}', current_tag)
+                            print(f'non_standard_tag: {non_standard_tag}')
                             if (
                                 non_standard_tag is None and current_tag != device_info['tag'] or 
                                 non_standard_tag is not None and device_info['tag'] in ['jetson51', 'jetson60', 'jetson61'] and non_standard_tag != device_info['tag']
                             ):
                                 try:
+                                    torch_version_base = Version(torch_version).base_version
+                                    print(f"{device_info['name']} hardware found! Installing the right torch library...")
                                     os = device_info['os']
                                     arch = device_info['arch']
                                     tag = device_info['tag']
