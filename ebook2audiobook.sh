4ecb0bac27aaa7d6190c9e54d93ec05a02be1086
unknown <admin@radsl.net>
2025-12-01 18:45:03 -0800

    ...

--- ebook2audiobook.sh
+++ ebook2audiobook.sh
@@ -2,7 +2,7 @@
 
 if [[ "$OSTYPE" == darwin* && -z "$SWITCHED_TO_ZSH" && "$(ps -p $$ -o comm=)" != "zsh" ]]; then
 	export SWITCHED_TO_ZSH=1
-	exec env zsh "$0" "$@"
+	exec /bin/zsh "$0" "${=@}"
 fi
 
 if [[ -n "$BASH_SOURCE" ]]; then
