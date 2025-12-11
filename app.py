d19b75b94ac7c207997d0ab4dfe662be3381ec87
unknown <admin@radsl.net>
2025-12-11 12:19:26 -0800

    ...

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ Tip: to add of silence (1.4 seconds) into your text just use "###" or "[pause]".
         if args['script_mode'] == NATIVE:
             from lib.classes.device_installer import DeviceInstaller
             manager = DeviceInstaller()
-            if manager.check_and_install_requirements():
+            if manager.install_python_packages():
                 device_info_str = manager.check_device_info(args['script_mode'])
                 if manager.install_device_packages(device_info_str) == 1:
                     error = f'Error: Could not installed device packages!'
