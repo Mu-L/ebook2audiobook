2630bd53558ea977fe0b1dcd00e151785fba59c5
unknown <admin@radsl.net>
2025-12-16 06:37:57 -0800

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -748,8 +748,8 @@ class DeviceInstaller():
                                         torch_pkg = f'{url}/{tag}/torch-{torch_version_base}%2B{tag}-{tag_py}-{os_env}_{arch}.whl'
                                         torchaudio_pkg = f'{url}/{tag}/torchaudio-{torch_version_base}%2B{tag}-{tag_py}-{os_env}_{arch}.whl'
                                         subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--no-cache-dir', torch_pkg, torchaudio_pkg])
-                                        if device_info['name'] == 'cuda':
-                                            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--no-cache-dir', '--no-build-isolation', 'deepspeed'])
+                                        #if device_info['name'] == 'cuda':
+                                        #    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--no-cache-dir', '--no-build-isolation', 'deepspeed'])
                                     #msg = 'Relaunching app...'
                                     #print(msg)
                                     #os.execv(sys.executable, [sys.executable] + sys.argv)
