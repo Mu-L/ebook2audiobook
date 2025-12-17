79002b64a578c635a77ec7df00e1b4eb4d00f767
unknown <admin@radsl.net>
2025-12-16 20:33:44 -0800

    ...

--- lib/classes/background_detector.py
+++ lib/classes/background_detector.py
@@ -10,29 +10,27 @@ from lib.models import default_voice_detection_model
 class BackgroundDetector:
 
 	def __init__(self, wav_file:str):
-		self.wav_file = wav_file
+        self.wav_file = wav_file
 
-		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-		model = Model.from_pretrained(
-			default_voice_detection_model,
-			cache_dir=tts_dir
-		)
+        model = Model.from_pretrained(
+            default_voice_detection_model,
+            cache_dir=tts_dir
+        )
 
-		self.pipeline = VoiceActivityDetection(segmentation=model)
-        hyper_params={
-          # onset/offset activation thresholds
-          "onset":0.5,"offset":0.5,
-          # remove speech regions shorter than that many seconds.
-          "min_duration_on":0.0,
-          # fill non-speech regions shorter than that many seconds.
-          "min_duration_off":0.0
+        self.pipeline = VoiceActivityDetection(segmentation=model)
+        hyper_params = {
+            # onset/offset activation thresholds
+            "onset":0.5,"offset":0.5,
+            # remove speech regions shorter than that many seconds.
+            "min_duration_on":0.0,
+            # fill non-speech regions shorter than that many seconds.
+            "min_duration_off":0.0
         }
-		self.pipeline.instantiate(hyper_params)
-
-		self.pipeline.to(device)
-
-		self.total_duration = librosa.get_duration(path=self.wav_file)
+        self.pipeline.instantiate(hyper_params)
+        self.pipeline.to(device)
+        self.total_duration = librosa.get_duration(path=self.wav_file)
 
 	def detect(self, vad_ratio_thresh:float=0.05)->tuple[bool, dict[str, float | bool]]:
 		annotation = self.pipeline(self.wav_file)
