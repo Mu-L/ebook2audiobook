68ed88d011d474b090051f19e63b26d830e36eef
unknown <admin@radsl.net>
2025-12-17 20:54:43 -0800

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -731,8 +731,6 @@ class DeviceInstaller():
                                         torchaudio_pkg = f"{url}/v{toolkit_version}/torchaudio-{jetson_torch_version_base[tag]}%2B{tag}-{tag_py}-{os_env}_{arch}.whl"
                                         subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--no-cache-dir', torch_pkg])
                                         subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--no-cache-dir', torchaudio_pkg])
-                                        subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', '-y', 'scikit-learn'])
-                                        subprocess.check_call(['conda', 'install', '-d', 'conda-forge', 'scikit-learn',  '-y'])
                                     elif device_info['name'] == devices['MPS']['proc']:
                                         torch_tag_py = f'cp{default_py_major}{default_py_minor}-none'
                                         torchaudio_tag_py = f'cp{default_py_major}{default_py_minor}-cp{default_py_major}{default_py_minor}'
