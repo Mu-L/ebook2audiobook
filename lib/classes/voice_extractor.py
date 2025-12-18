19b6f0da9f53a77104ae16cc34766a2a3f8dcc26
unknown <admin@radsl.net>
2025-12-17 17:14:50 -0800

    ...

--- lib/classes/voice_extractor.py
+++ lib/classes/voice_extractor.py
@@ -103,23 +103,18 @@ class VoiceExtractor:
                     return True, msg
                 else:
                     error = f'_demucs_voice() error: {self.wav_file}'
-                    return False, error
             except subprocess.CalledProcessError as e:
                 error = (
                     f'_demucs_voice() subprocess CalledProcessError error: {e.returncode}\n\n'
                     f'stdout: {e.output}\n\n'
                     f'stderr: {e.stderr}'
                 )
-                raise ValueError(error)
             except FileNotFoundError:
                 error = f'_demucs_voice() subprocess FileNotFoundError error: The "demucs" command was not found. Ensure it is installed and in PATH.'
-                raise ValueError(error)
             except Exception as e:
                 error = f'_demucs_voice() subprocess Exception error: {str(e)}'
-                raise ValueError(error)
         except Exception as e:
             error = f'_demucs_voice() error: {e}'
-            raise ValueError(error)
         return False, error
 
     def _remove_silences(self, audio:AudioSegment, silence_threshold:int, min_silence_len:int = 200, keep_silence:int = 300)->None:
