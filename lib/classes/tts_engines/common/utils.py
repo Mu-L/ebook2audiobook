c39831385242bfeecf51892cb6f2cc6c13693788
unknown <admin@radsl.net>
2025-12-19 19:56:20 -0800

    ...

--- lib/classes/tts_engines/common/utils.py
+++ lib/classes/tts_engines/common/utils.py
@@ -12,7 +12,7 @@ from torch import Tensor
 from torch.nn import Module
 
 from lib.conf import tts_dir
-from lib.models import xtts_builtin_speakers_list, models
+from lib.models import xtts_builtin_speakers_list, TTS_ENGINES, models
 
 def cleanup_memory()->None:
     gc.collect()
