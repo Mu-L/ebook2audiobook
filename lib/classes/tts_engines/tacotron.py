2a86a3778d32631c7d63dc7b7f4593d3fc4642f0
unknown <admin@radsl.net>
2025-12-20 17:32:13 -0800

    ...

--- lib/classes/tts_engines/tacotron.py
+++ lib/classes/tts_engines/tacotron.py
@@ -1,15 +1,4 @@
-import os, torch, torchaudio, random, regex as re, numpy as np
-
-from multiprocessing.managers import DictProxy
-from typing import Any
-from pathlib import Path
-
-from lib.classes.tts_registry import TTSRegistry
-from lib.classes.tts_engines.common.utils import TTSUtils
-from lib.classes.tts_engines.common.audio import trim_audio, is_audio_data_valid
-from lib.conf import tts_dir, devices, default_audio_proc_format
-from lib.conf_models import TTS_ENGINES, TTS_SML, loaded_tts, default_vc_model, models
-from lib.conf_lang import language_tts
+from lib.classes.tts_engines.common.headers import *
 
 class Tacotron2(TTSUtils, TTSRegistry, name='tacotron'):
 
