996041a67cd78a6c2de70f4ba810edd4e2440836
unknown <admin@radsl.net>
2025-12-22 16:59:58 -0800

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1556,8 +1556,7 @@ def build_interface(args:dict)->gr.Blocks:
                 try:
                     nonlocal models
                     msg = 'Error while loading saved session. Please try to delete your cookies and refresh the page'
-                    print(f"------------------{data.get('id', False)}-------------")
-                    if data and not isinstance(data, str) and data.get('id', False):
+                    if not data or isinstance(data, str) or not data.get('id', False):
                         session = context.set_session(str(uuid.uuid4()))
                     else:
                         session = context.get_session(data.get('id'))
