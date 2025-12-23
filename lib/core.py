f1f49004fb536c3152eae60cb05e46359635fab2
unknown <admin@radsl.net>
2025-12-22 19:18:15 -0800

    ...

--- lib/core.py
+++ lib/core.py
@@ -204,7 +204,7 @@ class SessionContext:
         if id in self.sessions:
             return self.sessions[id]
         error = 'get_session() error: session expired!'
-        DependencyError(error)
+        raise ValueError(error)
 
     def find_id_by_hash(self, socket_hash:str)->str|None:
         for id, session in self.sessions.items():
