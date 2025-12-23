cf05e571f61d49d56e07352be0701cfb32b5ac96
unknown <admin@radsl.net>
2025-12-23 15:13:18 -0800

    ...

--- lib/core.py
+++ lib/core.py
@@ -515,7 +515,7 @@ def convert2epub(id:str)-> bool:
                 with open(file_input, 'r', encoding='utf-8') as f:
                     text = f.read()
                 text = text.replace('\r\n', '\n')
-                text = text.replace('\n\n', '\n[[pause]]\n')
+                text = text.replace('\n\n', '\n<span data-pause="1"></span>\n')
                 with open(file_input, 'w', encoding='utf-8') as f:
                     f.write(text)
             elif file_ext == '.pdf':
