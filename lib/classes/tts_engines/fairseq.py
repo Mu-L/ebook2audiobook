a744826ac9db7f3ac83480736dbc5f33beadaa9d
unknown <admin@radsl.net>
2026-01-01 18:50:36 -0800

    ...

--- lib/classes/tts_engines/fairseq.py
+++ lib/classes/tts_engines/fairseq.py
@@ -91,7 +91,7 @@ class Fairseq(TTSUtils, TTSRegistry, name='fairseq'):
                 else:
                     if sentence.endswith("'"):
                         sentence = sentence[:-1]
-                    trim_audio_buffer = 0.004
+                    trim_audio_buffer = 0.003
                     speaker_argument = {}
                     not_supported_punc_pattern = re.compile(r"[.:—]")
                     if self.params['voice_path'] is not None:
