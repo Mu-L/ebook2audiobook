a8af2fbebf94450dae146eafb98838b308ca03ea
unknown <admin@radsl.net>
2026-01-02 18:12:04 -0800

    ...

--- lib/classes/tts_engines/yourtts.py
+++ lib/classes/tts_engines/yourtts.py
@@ -105,9 +105,7 @@ class YourTTS(TTSUtils, TTSRegistry, name='yourtts'):
                 print(sentence_parts)
                 for part in sentence_parts:
                     part = part.strip()
-                    if not part:
-                        continue
-                    if not part.replace('—', '') or not part.isalnum() or len(part) < 3:
+                    if not part or ((part and not part.replace('—', '')) or not part.isalnum() or len(part) < 3):
                         continue
                     if default_sml_pattern.fullmatch(part):
                         if not self.convert_sml(part):
