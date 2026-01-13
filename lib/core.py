3bf457d45533207114362570dab54c733df52b70
unknown <admin@radsl.net>
2026-01-12 19:01:52 -0800

    ...

--- lib/core.py
+++ lib/core.py
@@ -2512,7 +2512,7 @@ def convert_ebook(args:dict)->tuple:
                                 if session['free_vram_gb'] > 4.0:
                                     if session['tts_engine'] == TTS_ENGINES['BARK']:
                                         os.environ['SUNO_USE_SMALL_MODELS'] = 'False'                        
-                            if session['device'] == devices['CUDA']['proc']:
+                            if session['device'] == devices['CUDA']['proc'] or ession['device'] == devices['JETSON']['proc']:
                                 session['device'] = session['device'] if devices['CUDA']['found'] else devices['CPU']['proc']
                                 if session['device'] == devices['CPU']['proc']:
                                     msg += f'CUDA not supported by the Torch installed!<br/>Read {default_gpu_wiki}<br/>Switching to CPU'
