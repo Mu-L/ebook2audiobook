6abc50f452c24d6c45d3b0fb7b743401e4dff322
unknown <admin@radsl.net>
2026-01-13 20:53:21 -0800

    ...

--- lib/core.py
+++ lib/core.py
@@ -10,7 +10,7 @@ from __future__ import annotations
 import argparse, asyncio, csv, fnmatch, hashlib, io, json, math, os, pytesseract, gc
 import platform, random, shutil, subprocess, sys, tempfile, threading, time, uvicorn
 import traceback, socket, unicodedata, urllib.request, uuid, zipfile, fitz, multiprocessing
-import ebooklib, gradio as gr, psutil, regex as re, requests, stanza, importlib
+import ebooklib, gradio as gr, psutil, regex as re, requests, stanza, importlib, queue
 
 from typing import Any
 from PIL import Image, ImageSequence
