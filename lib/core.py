2d45c79f4009e7f2be2f3894a8c79855e86e2fd1
unknown <admin@radsl.net>
2026-01-16 06:05:38 -0800

    ...

--- lib/core.py
+++ lib/core.py
@@ -374,7 +374,7 @@ def compare_checksums(src_path:str, checksum_path:str, hash_algorithm:str='sha25
             return True, None
         with open(checksum_path, 'w', encoding='utf-8') as f:
             f.write(new_checksum)
-        return False, None
+            return True, None
     except Exception as e:
         return False, f'compare_checksums() error: {e}'
 
