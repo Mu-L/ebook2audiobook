eeda9f10a8d2c9155be37bb179618ad023f625d6
unknown <admin@radsl.net>
2026-01-15 17:30:32 -0800

    ...

--- lib/classes/tts_engines/presets/tacotron_presets.py
+++ lib/classes/tts_engines/presets/tacotron_presets.py
@@ -3,10 +3,10 @@ from lib.conf_models import TTS_ENGINES, default_engine_settings
 models = {
    "internal": {
         "lang": "multi",
-        "repo": "tts_models/[lang_iso1]/[xxx]",
+        "repo": "wietsedv/tacotron2-dutch",
         "sub": {
             "mai/tacotron2-DDC": ['fr', 'es'],
-            "css10/vits": ['nl'],
+            "wietsedv/tacotron2-dutch": ['nl'],
             "thorsten/tacotron2-DDC": ['de'],
             "kokoro/tacotron2-DDC": ['ja'],
             "ljspeech/tacotron2-DDC": ['en'],
@@ -16,7 +16,7 @@ models = {
         "files": default_engine_settings[TTS_ENGINES['TACOTRON2']]['files'],
         "samplerate": {
             "mai/tacotron2-DDC": default_engine_settings[TTS_ENGINES['TACOTRON2']]['samplerate'],
-            "css10/vits": default_engine_settings[TTS_ENGINES['TACOTRON2']]['samplerate'],
+            "wietsedv/tacotron2-dutch": default_engine_settings[TTS_ENGINES['TACOTRON2']]['samplerate'],
             "thorsten/tacotron2-DDC": default_engine_settings[TTS_ENGINES['TACOTRON2']]['samplerate'],
             "kokoro/tacotron2-DDC": default_engine_settings[TTS_ENGINES['TACOTRON2']]['samplerate'],
             "ljspeech/tacotron2-DDC": default_engine_settings[TTS_ENGINES['TACOTRON2']]['samplerate'],
