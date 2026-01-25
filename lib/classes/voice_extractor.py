6c5df849018f67ce4588412feb3d11369ea7f1fd
unknown <admin@radsl.net>
2026-01-24 20:07:32 -0800

    ...

--- lib/classes/voice_extractor.py
+++ lib/classes/voice_extractor.py
@@ -97,8 +97,6 @@ class VoiceExtractor:
         try:
             msg = 'Extracting'
             print(msg)
-            if self.is_gui_process:
-                self.progress_bar(0, desc=msg)
             cmd = [
                 "demucs",
                 "--verbose",
