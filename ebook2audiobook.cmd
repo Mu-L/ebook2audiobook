aca1c94cac3774945dbde054440273befc192f1a
unknown <admin@radsl.net>
2026-02-12 05:31:06 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -459,7 +459,7 @@ exit /b 0
 :check_device_info
 set "ARG=%~1"
 set "device_info_str="
-%PS_EXE% %PS_ARGS% -Command "python -c \"import sys; from lib.classes.device_installer import DeviceInstaller; device = DeviceInstaller(); result = device.check_device_info(r''%ARG%''); print(result if result else '')\""
+"%PS_EXE%" %PS_ARGS% -Command "python -c \"import sys; from lib.classes.device_installer import DeviceInstaller; device = DeviceInstaller(); result = device.check_device_info(r'%ARG%'); print(result if result else '')\""
 pause
 if not defined device_info_str exit /b 1
 exit /b 0
