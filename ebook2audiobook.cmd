cb0cc06624952f106681e0c89ca8e18ce0603e13
unknown <admin@radsl.net>
2026-02-12 07:01:02 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -633,8 +633,7 @@ if defined arguments.help (
 			setlocal DisableDelayedExpansion
 			call :check_device_info "%SCRIPT_MODE%"
 			endlocal & set "device_info_str=%device_info_str%"
-			echo errorlevel
-			echo %device_info_str%
+			echo !device_info_str!
 			pause
 			if errorlevel 1 goto :failed
 			if defined DEVICE_TAG (
