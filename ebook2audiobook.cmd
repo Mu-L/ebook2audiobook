2ccc0e5e6bdf66b2461adcf60876ff889a3b2c7a
unknown <admin@radsl.net>
2026-02-12 04:54:53 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -458,9 +458,15 @@ exit /b 0
 
 :check_device_info
 set "ARG=%~1"
+set "device_info_str="
+for /f "usebackq delims=" %%I in (`
 "%PS_EXE%" %PS_ARGS% -Command ^
-"python -c \"import sys; from lib.classes.device_installer import DeviceInstaller; device = DeviceInstaller(); result = device.check_device_info(r'%ARG%'); print(result) if result else None; sys.exit(0 if result else 1)\""
-exit /b %errorlevel%
+"python -c \"import sys; from lib.classes.device_installer import DeviceInstaller; device = DeviceInstaller(); result = device.check_device_info(r'%ARG%'); print(result if result else '')\""
+`) do (
+	set "device_info_str=%%I"
+)
+if not defined device_info_str exit /b 1
+exit /b 0
 
 :install_python_packages
 echo [ebook2audiobook] Installing dependencies…
@@ -630,10 +636,7 @@ if defined arguments.help (
             if errorlevel 1	goto :install_programs
 			set "device_info_str="
 			call :check_device_info "%SCRIPT_MODE%"
-			if "!device_info_str!"=="" (
-				echo error: check_device_info result is empty
-				goto :failed
-			)
+			if errorlevel 1 goto :failed
 			if defined DEVICE_TAG (
 				set "TAG=!DEVICE_TAG!"
 			) else (
