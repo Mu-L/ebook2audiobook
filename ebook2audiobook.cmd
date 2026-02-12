4d95d661f032d30b54171ce71b03dc3da54b373d
unknown <admin@radsl.net>
2026-02-11 16:57:42 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -298,7 +298,7 @@ if not "%OK_DOCKER%"=="0" (
 		call "%PS_EXE%" %PS_ARGS% -Command "scoop install rancher-desktop"
 		if exist "%SCOOP_APPS%\rancher-desktop\current\resources\resources\win32\bin\docker.exe" (
 			call "%PS_EXE%" -NoLogo -NoProfile -Command "$env:PATH = [Environment]::GetEnvironmentVariable('PATH','User') + ';' + [Environment]::GetEnvironmentVariable('PATH','Machine')"
-			start "" cmd /k "cd /d ""%SAFE_SCRIPT_DIR%"" ^& call ""%~f0"""
+			start "" cmd /k "cd /d %SAFE_SCRIPT_DIR% ^& call %~f0"
 			exit
 		)
 		where docker >nul 2>&1
