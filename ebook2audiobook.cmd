05179824ed5840edc001be42cac042e91ea6e53e
unknown <admin@radsl.net>
2026-02-13 16:14:14 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -748,11 +748,7 @@ start "%APP_NAME%" cmd /k "cd /d ""%SAFE_SCRIPT_DIR%"" & call %APP_FILE% %ARGS%"
 exit
 
 :restart_script_admin
-set "ELEVATE_VBS=%TEMP%\elevate_%RANDOM%.vbs"
-echo Set UAC = CreateObject("Shell.Application") > "%ELEVATE_VBS%"
-echo UAC.ShellExecute "%~f0", "%ARGS%", "", "runas", 1 >> "%ELEVATE_VBS%"
-cscript //nologo "%ELEVATE_VBS%"
-del "%ELEVATE_VBS%"
+call "%PS_EXE%" -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command Start-Process -FilePath "%~f0" -ArgumentList "%ARGS%" -Verb RunAs
 exit
 
 endlocal
