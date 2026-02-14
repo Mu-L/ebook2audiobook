86245c15fc094fdd35b29adde33b054bbbeabffe
unknown <admin@radsl.net>
2026-02-13 18:28:58 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -529,9 +529,6 @@ exit /b 0
 :check_device_info
 set "ARG=%~1"
 for /f "delims=" %%I in ('python -c "import sys; from lib.classes.device_installer import DeviceInstaller as D; r=D().check_device_info(sys.argv[1]); print(r if r else '')" "%ARG%"') do set "DEVICE_INFO_STR=%%I"
-if not defined DEVICE_TAG (
-	for /f "delims=" %%I in ('echo(%DEVICE_INFO_STR%^| python -c "import json, sys; print(json.loads(sys.stdin.read())['tag'])"') do set "DEVICE_TAG=%%I"
-)
 exit /b
 
 :install_python_packages
