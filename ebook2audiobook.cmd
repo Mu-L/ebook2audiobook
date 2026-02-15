33e61e0fa39971aa0d615c7df277079f10bebf28
unknown <admin@radsl.net>
2026-02-15 04:59:08 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -692,7 +692,7 @@ if "%HAS_PODMAN_COMPOSE%"=="0" (
     if errorlevel 1 exit /b 1
 ) else (
 	echo Using docker build
-    docker buildx build --progress=plain --no-cache ^
+    echo docker buildx build --progress=plain --no-cache ^
         --build-arg PYTHON_VERSION="%py_vers%" ^
         --build-arg APP_VERSION="%APP_VERSION%" ^
         --build-arg DEVICE_TAG="%DEVICE_TAG%" ^
