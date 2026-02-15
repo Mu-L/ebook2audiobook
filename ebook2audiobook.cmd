863fa196895687f95435726efab47291c809ad31
unknown <admin@radsl.net>
2026-02-14 18:14:32 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -720,6 +720,7 @@ if defined arguments.help (
             call :check_docker
             if errorlevel 1	goto :install_programs
 			call :check_device_info %SCRIPT_MODE%
+			echo %DEVICE_INFO_STR%
 			if "%DEVICE_INFO_STR%"=="" goto :failed
 			if "%DEVICE_TAG%"=="" (
 				call :json_get tag
