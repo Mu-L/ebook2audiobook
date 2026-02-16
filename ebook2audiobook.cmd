56f4da1375d1fc5b32ceff1a6946eb3c883dbba0
unknown <admin@radsl.net>
2026-02-16 06:11:56 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -809,14 +809,13 @@ if defined arguments.help (
 			call :check_docker_daemon
 			if errorlevel 1 goto :failed
 			call :check_device_info %SCRIPT_MODE%
-			set "DEVICE_INFO_STR=!DEVICE_INFO_STR!"
 			if errorlevel 1 goto :failed
 			if "%DEVICE_TAG%"=="" (
 				call :json_get tag
 				if errorlevel 1 goto :failed
 				set "DEVICE_TAG=%JSON_VALUE%"
 			)
-			echo device info string: %DEVICE_INFO_STR%
+			echo device info string: !DEVICE_INFO_STR!
 			echo device tag: !DEVICE_TAG!
 			docker image inspect "%DOCKER_IMG_NAME%:%DEVICE_TAG%" >nul 2>&1
 			if not errorlevel 1 (
@@ -824,7 +823,7 @@ if defined arguments.help (
 				echo Delete it using: docker rmi %DOCKER_IMG_NAME%:%DEVICE_TAG% --force
 				goto :failed
 			)
-            call :build_docker_image "%DEVICE_INFO_STR%"
+            call :build_docker_image "!DEVICE_INFO_STR!"
             if errorlevel 1 goto :failed
         ) else (
             call :install_python_packages
