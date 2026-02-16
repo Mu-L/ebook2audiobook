8271e798adc3f407dd92458079b50c5998232aeb
unknown <admin@radsl.net>
2026-02-16 06:24:03 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -815,11 +815,10 @@ if defined arguments.help (
 				if errorlevel 1 goto :failed
 				set "DEVICE_TAG=!JSON_VALUE!"
 			)
-			echo DEVICE_TAG: !DEVICE_TAG!
-			docker image inspect "%DOCKER_IMG_NAME%:%DEVICE_TAG%" >nul 2>&1
+			docker image inspect "%DOCKER_IMG_NAME%:!DEVICE_TAG!" >nul 2>&1
 			if not errorlevel 1 (
-				echo [STOP] Docker image "%DOCKER_IMG_NAME%:%DEVICE_TAG%" already exists. Aborting build.
-				echo Delete it using: docker rmi %DOCKER_IMG_NAME%:%DEVICE_TAG% --force
+				echo [STOP] Docker image "%DOCKER_IMG_NAME%:!DEVICE_TAG!" already exists. Aborting build.
+				echo Delete it using: docker rmi %DOCKER_IMG_NAME%:!DEVICE_TAG! --force
 				goto :failed
 			)
             call :build_docker_image "!DEVICE_INFO_STR!"
