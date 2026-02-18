e9483aa244d6f6250b61ec563f77dd9a97d5dced
unknown <admin@radsl.net>
2026-02-18 13:49:29 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -311,7 +311,7 @@ if not "%OK_WSL%"=="0" (
 		wsl --install -d Ubuntu
 		ubuntu config --default-user root
 		echo [wsl2] > "%USERPROFILE%\.wslconfig"
-		echo automount=false >> "%USERPROFILE%\.wslconfig"
+		echo memory=4GB >> "%USERPROFILE%\.wslconfig"
 		echo.
 		echo ==================================================
 		echo WSL installation triggered.
@@ -329,8 +329,8 @@ if not "%OK_DOCKER%"=="0" (
             echo %ESC%[31m=============== docker install failed.%ESC%[0m
             goto :failed
         )
-        wsl --user root -d Ubuntu -- bash -c "echo '[boot]' > /etc/wsl.conf && echo 'systemd=true' >> /etc/wsl.conf"
-        wsl --shutdown
+		wsl --user root -d Ubuntu -- bash -c "echo '[boot]' > /etc/wsl.conf && echo 'systemd=true' >> /etc/wsl.conf"
+		wsl --shutdown
         echo %ESC%[33m=============== docker OK ===============%ESC%[0m
         goto :restart_script
     )
@@ -634,7 +634,6 @@ if /i "%DEVICE_TAG%"=="cpu" (
 for /f "delims=" %%i in ('wsl --user root -d Ubuntu -- wslpath "%SAFE_SCRIPT_DIR:\=/%"') do set "WSL_DIR=%%i"
 call :get_iso3_lang "%OS_LANG%"
 set "ISO3_LANG=!ISO3_LANG!"
-wsl --user root -d Ubuntu -- bash -c "cd '%WSL_DIR%' && docker buildx use wslbuilder"
 if "%HAS_PODMAN_COMPOSE%"=="0" (
     echo Using podman-compose
     set "PODMAN_BUILD_ARGS=--format docker --no-cache --network=host"
@@ -651,11 +650,21 @@ if "%HAS_PODMAN_COMPOSE%"=="0" (
 ) else if "%HAS_COMPOSE%"=="0" (
     echo Using docker-compose
     wsl --user root -d Ubuntu -- bash -c "cd '%WSL_DIR%' && docker compose --progress=plain --profile %COMPOSE_PROFILES% build --no-cache --build-arg PYTHON_VERSION='%py_vers%' --build-arg APP_VERSION='%APP_VERSION%' --build-arg DEVICE_TAG='%DEVICE_TAG%' --build-arg DOCKER_DEVICE_STR='%ARG_ESCAPED%' --build-arg DOCKER_PROGRAMS_STR='%DOCKER_PROGRAMS%' --build-arg CALIBRE_INSTALLER_URL='%DOCKER_CALIBRE_INSTALLER_URL%' --build-arg ISO3_LANG='%ISO3_LANG%'"
-    if errorlevel 1 endlocal & exit /b 1    if errorlevel 1 endlocal & exit /b 1
+    if errorlevel 1 endlocal & exit /b 1
 ) else (
     echo Using docker buildx
-    wsl --user root -d Ubuntu -- bash -c "cd '%WSL_DIR%' && docker buildx build --progress=plain --no-cache --platform linux/amd64 --build-arg PYTHON_VERSION='%py_vers%' --build-arg APP_VERSION='%APP_VERSION%' --build-arg DEVICE_TAG='%DEVICE_TAG%' --build-arg DOCKER_DEVICE_STR='%ARG_ESCAPED%' --build-arg DOCKER_PROGRAMS_STR='%DOCKER_PROGRAMS%' --build-arg CALIBRE_INSTALLER_URL='%DOCKER_CALIBRE_INSTALLER_URL%' --build-arg ISO3_LANG='%ISO3_LANG%' -t '%DOCKER_IMG_NAME%' ."
-    if errorlevel 1 endlocal & exit /b 1
+    wsl --user root -d Ubuntu -- bash -c "cd '%WSL_DIR%' && docker buildx use wslbuilder"
+    if errorlevel 1 (
+        echo Failed to switch to wslbuilder!
+        endlocal 
+        exit /b 1
+    )
+    wsl --user root -d Ubuntu -- bash -c "cd '%WSL_DIR%' && docker buildx build --shm-size=4g --progress=plain --no-cache --platform linux/amd64 --build-arg PYTHON_VERSION='%py_vers%' --build-arg APP_VERSION='%APP_VERSION%' --build-arg DEVICE_TAG='%DEVICE_TAG%' --build-arg DOCKER_DEVICE_STR='%ARG_ESCAPED%' --build-arg DOCKER_PROGRAMS_STR='%DOCKER_PROGRAMS%' --build-arg CALIBRE_INSTALLER_URL='%DOCKER_CALIBRE_INSTALLER_URL%' --build-arg ISO3_LANG='%ISO3_LANG%' -t '%DOCKER_IMG_NAME%' ."
+    if errorlevel 1 (
+        echo Build failed!
+        endlocal 
+        exit /b 1
+    )
 )
 if defined cmd_options set "cmd_extra=%cmd_options% "
 echo Docker image ready. To run your docker:
