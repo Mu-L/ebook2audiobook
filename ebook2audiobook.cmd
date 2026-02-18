87f465423291092fc81a554fc14c68f111c1d328
unknown <admin@radsl.net>
2026-02-18 14:01:50 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -753,21 +753,25 @@ if defined arguments.help (
             )
             call :check_docker
             if errorlevel 1	goto :install_programs
-			call :check_docker_daemon
-			if errorlevel 1 goto :failed
-			call :check_device_info %SCRIPT_MODE%
-			if errorlevel 1 goto :failed
-			if "%DEVICE_TAG%"=="" (
-				call :json_get tag
-				if errorlevel 1 goto :failed
-				set "DEVICE_TAG=!JSON_VALUE!"
-			)
-			docker image inspect "%DOCKER_IMG_NAME%:!DEVICE_TAG!" >nul 2>&1
-			if not errorlevel 1 (
-				echo [STOP] Docker image "%DOCKER_IMG_NAME%:!DEVICE_TAG!" already exists. Aborting build.
-				echo Delete it using: docker rmi %DOCKER_IMG_NAME%:!DEVICE_TAG! --force
-				goto :failed
-			)
+            call :check_docker_daemon
+            if errorlevel 1 goto :failed
+            call :check_device_info %SCRIPT_MODE%
+            if errorlevel 1 goto :failed
+            if "%DEVICE_TAG%"=="" (
+                call :json_get tag
+                if errorlevel 1 goto :failed
+                set "DEVICE_TAG=!JSON_VALUE!"
+            )
+            if "%DOCKER_DESKTOP%"=="1" (
+                docker image inspect "%DOCKER_IMG_NAME%:!DEVICE_TAG!" >nul 2>&1
+            ) else (
+                wsl --user root -d Ubuntu -- docker image inspect "%DOCKER_IMG_NAME%:!DEVICE_TAG!" >nul 2>&1
+            )
+            if not errorlevel 1 (
+                echo [STOP] Docker image "%DOCKER_IMG_NAME%:!DEVICE_TAG!" already exists. Aborting build.
+                echo Delete it using: docker rmi %DOCKER_IMG_NAME%:!DEVICE_TAG! --force
+                goto :failed
+            )
             call :build_docker_image "!DEVICE_INFO_STR!"
             if errorlevel 1 goto :failed
         ) else (
