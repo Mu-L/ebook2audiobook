dc3810fef9f8f9c73f6e609d28bfc46c0d78505c
unknown <admin@radsl.net>
2026-02-27 05:22:08 -0800

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -451,13 +451,13 @@ def build_interface(args:dict)->gr.Blocks:
                 .blocks-buttons {
                     display: flex !important;
                     justify-content: space-evenly !important;
-                    margin-top: 20px !important;
-                    margin-bottom: 20px !important;
+                    margin-top: 12px !important;
+                    margin-bottom: 12px !important;
                 }
                 .blocks-buttons button {
-                    padding: 10px 20px !important;
+                    padding: 12px !important;
                     border: none !important;
-                    border-radius: 12px !important;
+                    border-radius: 9px !important;
                     font-size: 16px !important;
                     cursor: pointer !important;
                 }
@@ -703,9 +703,9 @@ def build_interface(args:dict)->gr.Blocks:
 
                 with gr.Row(elem_id='gr_row_buttons', visible=True) as gr_row_buttons:
                     gr.Column(scale=1)
-                    gr_blocks_cancel = gr.Button('✖', variant='stop', scale=0, size='md')
+                    gr_blocks_cancel = gr.Button('✖', elem_classes=['blocks-buttons'], variant='stop', scale=0, size='md')
                     gr.Column(scale=0, min_width=100)
-                    gr_blocks_continue = gr.Button('✔', variant='primary', scale=0, size='md')
+                    gr_blocks_continue = gr.Button('✔', elem_classes=['blocks-buttons'], variant='primary', scale=0, size='md')
                     gr.Column(scale=1)
 
             gr_version_markdown = gr.Markdown(elem_id='gr_version_markdown', value=f'''
