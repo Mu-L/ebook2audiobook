e95e7571a6131130f5b6db8267603f694ba07915
unknown <admin@radsl.net>
2026-03-03 05:17:59 -0800

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -29,6 +29,7 @@ class DeviceInstaller():
         os_env = None
         if mode == NATIVE:
             name, tag, msg = self.check_hardware
+            msg = 'cacaca'
             arch = self.check_arch
             pyvenv = list(sys.version_info[:2])
             os_env = 'linux' if name == devices['JETSON']['proc'] else self.check_platform
