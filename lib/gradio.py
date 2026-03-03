6a258d9aed34e9cc21d79d6df543a9b2a929101b
unknown <admin@radsl.net>
2026-03-03 08:09:49 -0800

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -4,7 +4,7 @@ from lib.classes.tts_engines.common.preset_loader import load_engine_presets
 progress_bar = None
 
 def build_interface(args:dict)->gr.Blocks:
-    nonlocal progress_bar
+    global progress_bar
     try:
         script_mode = args['script_mode']
         is_gui_process = args['is_gui_process']
