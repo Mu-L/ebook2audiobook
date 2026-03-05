7be6373069116dc4d7d2fa90f41611b874bf48e3
unknown <admin@radsl.net>
2026-03-05 04:36:18 -0800

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -2277,7 +2277,7 @@ def build_interface(args:dict)->gr.Blocks:
                 outputs=outputs_refresh_interface
             )
             gr_override_confirm_btn.click(
-                fn=lambda: :click_gr_override_buttons(confirmed=True),
+                fn=lambda: click_gr_override_buttons(confirmed=True),
                 inputs=None,
                 outputs=[gr_modal, gr_event]
             )
