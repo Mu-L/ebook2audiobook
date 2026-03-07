5335f46d126055ffd02be66c7954e8c687e56c7f
unknown <admin@radsl.net>
2026-03-07 06:44:55 -0800

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -35,23 +35,17 @@ class DeviceInstaller():
         else:
             if mode == BUILD_DOCKER:
                 name, tag, msg = self.check_hardware
+                os_env = 'manylinux_2_28'
                 pyvenv = list(sys.version_info[:2])
                 pyvenv = [3, 10] if tag in ['jetson51', 'jetson60', 'jetson61'] else pyvenv
                 arch = 'aarch64' if name in [devices['JETSON']['proc']] else self.check_arch
                 tag = 'cpu' if name in [devices['JETSON']['proc'], devices['MPS']['proc']] else tag
-                os_env = 'manylinux_2_28'
-                device_info = {"name":name,"os":os_env,"arch":arch,"pyvenv":pyvenv,"tag":tag,"note":msg.replace('!', '')}
+                device_info = {"name": name, "os": os_env, "arch": arch, "pyvenv": pyvenv, "tag": tag, "note": msg.replace('!', '')}
                 with open('.device_info.json', 'w', encoding='utf-8') as f:
                     json.dump(device_info, f)
                 return json.dumps(device_info)
-            elif mode == FULL_DOCKER:
-                try:
-                    with open('.device_info.json', 'r', encoding='utf-8') as f:
-                        return json.dumps(json.load(f))
-                except FileNotFoundError:
-                    return ''
         if all([name, tag, os_env, arch, pyvenv]):
-            device_info = {"name":name,"os":os_env,"arch":arch,"pyvenv":pyvenv,"tag":tag,"note":msg}
+            device_info = {"name": name, "os": os_env, "arch": arch, "pyvenv": pyvenv, "tag": tag, "note": msg}
             return json.dumps(device_info)
         return ''
         
