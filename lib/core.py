88e54941e1fb24c44c22e5289bda7cf3b5009390
unknown <admin@radsl.net>
2026-03-08 17:41:40 -0700

    ...

--- lib/core.py
+++ lib/core.py
@@ -2548,6 +2548,7 @@ def convert_ebook(args:dict)->tuple:
         session_id = None
         info_session = None
         if args['language'] is not None:
+            print(f"============== {args['output_dir']}")
             if not os.path.splitext(args['ebook'])[1]:
                 error = f"{args['ebook']} needs a format extension."
                 print(error)
@@ -2618,7 +2619,6 @@ def convert_ebook(args:dict)->tuple:
             session['sentences_dir'] = os.path.join(session['chapters_dir'], 'sentences')
             cleanup_models_cache()
             if not session['is_gui_process']:
-                print(f"============== {args['output_dir']}")
                 session['system'] = DEVICE_SYSTEM
                 session['session_dir'] = os.path.join(tmp_dir, f'proc-{session_id}')
                 session['audiobooks_dir'] = os.path.abspath(args['output_dir']) if args['output_dir'] else os.path.join(audiobooks_cli_dir, f'cli-{session_id}')
