386c589109ecdf1651c29d12c1d1def3e08738dd
unknown <admin@radsl.net>
2026-03-14 08:26:53 -0700

    ...

---
+++ lib/classes/std_filter.py
@@ -0,0 +1,23 @@
+import os, sys
+
+class StdoutFilter:
+    def __init__(self, original):
+        self._original = original
+
+    def write(self, msg):
+        if 'NNPACK' not in msg:
+            self._original.write(msg)
+
+    def flush(self):
+        self._original.flush()
+
+class StderrFilter:
+    def __init__(self, original):
+        self._original = original
+
+    def write(self, msg):
+        if 'NNPACK' not in msg:
+            self._original.write(msg)
+
+    def flush(self):
+        self._original.flush()
