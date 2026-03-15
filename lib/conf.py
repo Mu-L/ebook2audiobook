e8734aae42ff7f29e85183adbb20f81cfc862611
unknown <admin@radsl.net>
2026-03-14 18:28:06 -0700

    ...

--- lib/conf.py
+++ lib/conf.py
@@ -10,13 +10,13 @@ systems = {
     "WINDOWS": "win32"
 }
 
-command_options = [
+cli_options = [
     '--script_mode', '--docker_mode', '--session', '--share', '--headless', 
     '--ebook', '--ebooks_dir', '--language', '--voice', '--device', '--tts_engine', 
     '--custom_model', '--fine_tuned', '--output_format', '--output_channel',
     '--temperature', '--length_penalty', '--num_beams', '--repetition_penalty', 
-    '--top_k', '--top_p', '--speed', '--enable_text_splitting',
-    '--text_temp', '--waveform_temp', '--output_dir', '--version', '--workflow', '--help'
+    '--top_k', '--top_p', '--speed', '--enable_text_splitting', '--text_temp',
+    '--waveform_temp', '--output_dir', '--version', '--workflow', '--help', '--docker_device'
 ]
 
 workflow_id = 'ba800d22-ee51-11ef-ac34-d4ae52cfd9ce'
