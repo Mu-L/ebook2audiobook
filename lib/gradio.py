13a684e21e9f15ecfdaccf99072b6a737c02259c
unknown <admin@radsl.net>
2026-03-17 19:54:58 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -752,7 +752,8 @@ def build_interface(args:dict)->gr.Blocks:
                                     scale=0
                                 )
                                 gr.Column(scale=1)
-                                gr.Label(value='Reload original text', scale=0, min_width=100)
+                                with gr.Column(scale=0, min_width=120):
+                                        gr.Markdown('Reload original text')
                                 gr_block_reset = gr.Button(
                                     '↺',
                                     elem_id=f'gr_block_reset_{i}',
