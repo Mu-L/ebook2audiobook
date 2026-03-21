1d1a255b4275ed37a01aeb251d3a0db35f19fa9b
unknown <admin@radsl.net>
2026-03-21 06:22:58 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1115,8 +1115,7 @@ def build_interface(args:dict)->gr.Blocks:
                 try:
                     session = context.get_session(session_id)
                     if session and session.get('id', False):
-                        session['ebook'] = None
-                        session['ebook_list'] = None
+                        reset_ebook_session(session_id, True)
                         if data is None:
                             if session.get('status', None) == status_tags['CONVERTING']:
                                 session['cancellation_requested'] = True
