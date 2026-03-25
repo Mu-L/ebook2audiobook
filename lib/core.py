006d5d12566fadaa0ba025eec5197f5c8d86d4d0
unknown <admin@radsl.net>
2026-03-24 18:29:06 -0700

    ...

--- lib/core.py
+++ lib/core.py
@@ -2927,6 +2927,7 @@ def finalize_audiobook(session_id:str)->tuple:
             sentences_list = get_sentences(session_id, block['text'])
             if sentences_list is None:
                 return result('No sentences found!', False)
+            print(f'---------------------idx: {idx}---------------')
             session['blocks_current']['blocks'][idx]['sentences'] = sentences_list
         print(session['blocks_current']['blocks'][0]['sentences'])
         conversion = convert_chapters2audio(session_id)
