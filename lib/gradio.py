c64bd09f6d63e3ebe1f5291c6547ec164d0d2f7c
unknown <admin@radsl.net>
2026-03-30 16:56:37 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1712,10 +1712,10 @@ def build_interface(args:dict)->gr.Blocks:
                             if isinstance(args['ebook_list'], list):
                                 for progress_status, passed in convert_ebook_directory(args):
                                     if passed:
-                                        if progress_status == status_tags['EDIT']:
+                                        if session['status'] == status_tags['EDIT']:
                                             return gr.update(value=progress_status)
                                         else:
-                                            if session['status'][ == status_tags['CONVERTING']:
+                                            if session['status'] == status_tags['CONVERTING']:
                                                 yield gr.update(value=progress_status)
                                             else:
                                                 reset_ebook_session(session_id, force=True, filter_keys=False)
