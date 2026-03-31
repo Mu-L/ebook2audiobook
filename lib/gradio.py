324a17b1461efbeefaab6bef4c4de7344d401ac6
unknown <admin@radsl.net>
2026-03-30 18:50:26 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1713,7 +1713,6 @@ def build_interface(args:dict)->gr.Blocks:
                                 for progress_status, passed in convert_ebook_directory(args):
                                     if passed:
                                         yield gr.update(value=progress_status)
-                                        return
                                     else:
                                         reset_ebook_session(args['id'], force=True, filter_keys=False)
                                         error = progress_status
