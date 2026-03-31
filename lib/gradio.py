a25cc088d63055cf9d872e79cb2d84120f70ccd5
unknown <admin@radsl.net>
2026-03-31 16:50:27 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1809,7 +1809,7 @@ def build_interface(args:dict)->gr.Blocks:
                             ebook_list.remove(session['ebook_src'])
                             session['ebook_list'] = ebook_list
                             return gr.update(value='', visible=False), gr.update(value=session['ebook_list'])
-                    session['status'] = status_tags['READY']
+                    session['status'] = status_tags['SKIP']
                 return gr.update(value='', visible=False), gr.update()
 
             def populate_page(session_id:str, page:int, blocks:list[dict])->tuple:
