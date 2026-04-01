63d2d0f9ec7dd7c6a2f049ea8d682e2b374ebf17
unknown <admin@radsl.net>
2026-03-31 19:19:23 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1787,11 +1787,11 @@ def build_interface(args:dict)->gr.Blocks:
                     if source is not None:
                         session['ebook_src'] = source
                         final_name = f"{get_sanitized(Path(source).stem)}{'_part1.' if session['output_split'] else '.'}{session['output_format']}"
-                        final_file = os.path.join(session['process_dir'], final_name)
                         process_dir = os.path.join(session['session_dir'], f"{hashlib.md5(os.path.join(session['audiobooks_dir'], final_name).encode()).hexdigest()}")
                         chapters_dir = os.path.join(process_dir, 'chapters')
                         sentences_dir = os.path.join(chapters_dir, 'sentences')
                         audio_sentences_exist = any(Path(sentences_dir).rglob(f'*.{default_audio_proc_format}')) if os.path.exists(sentences_dir) else False
+                        final_file = os.path.join(process_dir, final_name)
                         if os.path.exists(final_file) or audio_sentences_exist:
                             session['status'] = status_tags['OVERRIDE']
                             msg = f"Warning! the final file {final_name} of this conversion already exists. If you continue all new text and setting changes will override the previous conversion!"
