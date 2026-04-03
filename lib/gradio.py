23a323f3d380eb1a5835c3b3de650511aa548b4e
unknown <admin@radsl.net>
2026-04-03 11:46:28 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -2120,12 +2120,14 @@ def build_interface(args:dict)->gr.Blocks:
             outputs_enable_components = [
                 gr_ebook_mode, gr_blocks_preview, gr_language, gr_voice_file, gr_voice_list,
                 gr_device, gr_tts_engine_list, gr_fine_tuned_list, gr_custom_model_file,
-                gr_custom_model_list, gr_output_format_list, gr_output_channel_list, gr_modal, gr_convert_btn
+                gr_custom_model_list, gr_output_format_list, gr_output_channel_list, gr_modal, gr_convert_btn,
+                gr_voice_play, gr_voice_del_btn, gr_custom_model_del_btn, gr_session_closed_btn, gr_session_opened_btn
             ]
             outputs_disable_components = [
                 gr_ebook_mode, gr_blocks_preview, gr_language, gr_voice_file, gr_voice_list,
                 gr_device, gr_tts_engine_list, gr_fine_tuned_list, gr_custom_model_file,
-                gr_custom_model_list, gr_output_format_list, gr_output_channel_list, gr_convert_btn
+                gr_custom_model_list, gr_output_format_list, gr_output_channel_list, gr_convert_btn,
+                gr_voice_play, gr_voice_del_btn, gr_custom_model_del_btn, gr_session_closed_btn, gr_session_opened_btn
             ]
             outputs_edit_blocks = [
                 gr_blocks_markdown, gr_group_main, gr_group_blocks,
