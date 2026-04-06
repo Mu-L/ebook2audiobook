36d0cfed54d04fdd454979bc58a63ee77cfadab5
unknown <admin@radsl.net>
2026-04-05 19:13:42 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1783,7 +1783,8 @@ def build_interface(args:dict)->gr.Blocks:
                             "output_split":bool(output_split),
                             "output_split_hours": output_split_hours,
                         }
-                        if args['ebook_src'] is None and args['ebook_list'] is None:
+                        
+                        if args['ebook_mode'] != 'text' and args['ebook_src'] is None and args['ebook_list'] is None:
                             error = 'Error: a file or directory is required.'
                         elif args['xtts_num_beams'] < args['xtts_length_penalty']:
                             error = 'Error: num beams must be greater or equal than length penalty.'               
