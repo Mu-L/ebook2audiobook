49065ab6f92782ae26c453e25cb4db3586529008
unknown <admin@radsl.net>
2026-04-06 19:30:56 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -2279,6 +2279,12 @@ def build_interface(args:dict)->gr.Blocks:
                 inputs=[gr_session, gr_ebook_mode, gr_ebook_src],
                 outputs=[gr_modal],
                 show_progress_on=[gr_ebook_src]
+            ).then(
+                fn=lambda sid, f: gr.update(
+                    interactive=context.get_session(sid).get('status') == status_tags['READY'] and f is not None
+                ),
+                inputs=[gr_session, gr_ebook_src],
+                outputs=[gr_convert_btn],
             )
             gr_ebook_textarea.change(
                 fn=change_gr_ebook_textarea,
@@ -2787,6 +2793,10 @@ def build_interface(args:dict)->gr.Blocks:
                 inputs=[gr_session, gr_audiobook_list],
                 outputs=[gr_group_audiobook_list, gr_audiobook_player, gr_timer],
                 show_progress_on=[gr_progress]
+            ).then(
+                fn=lambda f: gr.update(interactive=True if f == 'text' else False),
+                inputs=[gr_ebook_mode],
+                outputs=[gr_convert_btn],
             ).then(
                 fn=lambda session: update_gr_glassmask(attr=['gr-glass-mask', 'hide']) if session else gr.update(),
                 inputs=[gr_session],
