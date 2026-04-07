e8cf47504f3b8fbe42b2762a8823c8b41659592f
unknown <admin@radsl.net>
2026-04-07 08:41:35 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -2666,6 +2666,15 @@ def build_interface(args:dict)->gr.Blocks:
                 inputs=[gr_session, gr_ebook_mode, gr_ebook_src, gr_ebook_textarea, gr_blocks_preview, gr_override_event],
                 outputs=[gr_modal, gr_override_event],
                 show_progress_on=[gr_progress]
+            ).then(
+                fn=lambda s: (
+                    enable_components(s)
+                    if context.get_session(s)['status'] == status_tags['READY']
+                    else None
+                ),
+                inputs=[gr_session],
+                outputs=outputs_enable_components
+                show_progress_on=[gr_progress]
             )
             gr_override_cancel_btn.click(
                 fn=click_gr_override_cancel_btn,
