4ea2541303a9f002c272a2bb8957522da4fba9c5
unknown <admin@radsl.net>
2026-04-08 11:46:30 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -2769,7 +2769,7 @@ def build_interface(args:dict)->gr.Blocks:
                         )
                     ).then(
                         fn=lambda s: (
-                            gr.update(value=None)
+                            gr.update(choices=[])
                             if context.get_session(s)['status'] in [status_tags['END'], status_tags['READY']]
                             else gr.update()
                         ),
@@ -2808,7 +2808,7 @@ def build_interface(args:dict)->gr.Blocks:
                         )
                     ).then(
                         fn=lambda s: (
-                            gr.update(value=[])
+                            gr.update(choices=[])
                             if context.get_session(s)['status'] in [status_tags['END'], status_tags['READY']]
                             else gr.update()
                         ),
