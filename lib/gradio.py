68d5f4c7145eb9801f32f4509730b162db46d9fa
unknown <admin@radsl.net>
2026-04-11 05:44:06 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -919,6 +919,7 @@ def build_interface(args:dict)->gr.Blocks:
             def enable_components(session_id:str)->tuple:
                 session = context.get_session(session_id)
                 if session and session.get('id', False):
+                    print(f"-------------------------- status: {session['status']} ---------------------------")
                     if session['status'] in [status_tags['READY'], status_tags['END']]:
                         session['status'] = status_tags['READY']
                         session['cancellation_requested'] = False
