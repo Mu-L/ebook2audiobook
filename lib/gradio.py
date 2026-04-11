15581ddcdbbbd6e3077a6f8fefad0000164868c1
unknown <admin@radsl.net>
2026-04-11 07:12:31 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -547,10 +547,10 @@ def build_interface(args:dict)->gr.Blocks:
                     font-size: 16px !important;
                     cursor: pointer !important;
                 }
-                .accordion-block-even, .accordion-block-even div textarea .wrap,
+                .accordion-block-even, .accordion-block-even div textarea .wrap {
                     background: var(--table-even-background-fill) !important;
                 }
-                .accordion-block-odd, .accordion-block-odd div textarea,
+                .accordion-block-odd, .accordion-block-odd div textarea, .wrap {
                     background: var(--table-odd-background-fill) !important;
                 }
                 .accordion-block-voice-list {
