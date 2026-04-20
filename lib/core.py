9ba39062d24b322280c0073e314b3f4979604b77
unknown <admin@radsl.net>
2026-04-19 17:20:33 -0700

    ...

--- lib/core.py
+++ lib/core.py
@@ -2202,8 +2202,9 @@ def convert_chapters2audio(session_id:str)->bool:
                             show_alert(session_id, {'type': 'warning', 'msg': error})
                             return False
                     voice_cache[old_voice] = new_voice
-                block['voice'] = new_voice
-                block['hash'] = block_hash(block)
+                if new_voice != old_voice:
+                    block['voice'] = new_voice
+                    block['hash'] = block_hash(block)
             session['blocks_current'] = blocks_current
         blocks_kept = [(i, b) for i, b in enumerate(blocks) if b['keep'] and b['text'].strip()]
         total_chapters = len(blocks_kept)
