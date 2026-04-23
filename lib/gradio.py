68b091c6c5ed97abec1c36da42d56d21a1127cd5
unknown <admin@radsl.net>
2026-04-23 05:49:02 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -2954,12 +2954,12 @@ def build_interface(args:dict)->gr.Blocks:
                             inputs=inputs_start_conversion,
                             outputs=[gr_progress],
                             show_progress_on=[gr_progress],
-                            js='()=>{window.load_vtt();}'
                         ).then(
                             fn=edit_blocks,
                             inputs=[gr_session],
                             outputs=outputs_edit_blocks,
-                            show_progress_on=[gr_progress]
+                            show_progress_on=[gr_progress],
+                            js='()=>{window.load_vtt();}'
                         )
                     )
                 ),
