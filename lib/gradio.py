cc55bf49cde1b4ef8942580b3c3960a1499725a9
unknown <admin@radsl.net>
2026-05-12 19:12:27 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1148,15 +1148,16 @@ def build_interface(args:dict)->gr.Blocks:
                         voice_file = session.get('voice')
                         translate_enabled_state = bool(session.get('translate_enabled'))
                         source_iso3 = session.get('language')
-                        translate_target_value = session.get('translate')
+                        translate_value = session.get('translate')
                         translate_options = _build_translate_targets(source_iso3) if source_iso3 else []
                         valid_target_codes = {o[1] for o in translate_options}
-                        if translate_target_value not in valid_target_codes:
-                            translate_target_value = translate_options[0][1] if translate_options else None
-                            session['translate'] = translate_target_value
-                            if translate_target_value:
+                        print(f'-------------{valid_target_codes}-------------')
+                        if translate_value not in valid_target_codes:
+                            translate_value = translate_options[0][1] if translate_options else None
+                            session['translate'] = translate_value
+                            if translate_value:
                                 try:
-                                    session['translate_iso1'] = Lang(translate_target_value).pt1
+                                    session['translate_iso1'] = Lang(translate_value).pt1
                                 except Exception:
                                     session['translate_iso1'] = None
                             else:
@@ -1170,7 +1171,7 @@ def build_interface(args:dict)->gr.Blocks:
                             gr.update(value=session['device']),
                             gr.update(value=session['language']),
                             gr.update(value=translate_enabled_state),
-                            gr.update(visible=translate_visible, choices=translate_options, value=session['translate']),
+                            gr.update(visible=translate_visible, choices=translate_options, value=translate_value),
                             update_gr_voice_list(session_id),
                             update_gr_tts_engine_list(session_id),
                             update_gr_custom_model_list(session_id),
