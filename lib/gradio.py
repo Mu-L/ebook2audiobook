c2226ce31803cb2380a70aafc91382e07bfef55f
unknown <admin@radsl.net>
2026-05-12 19:24:53 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -4031,7 +4031,7 @@ def build_interface(args:dict)->gr.Blocks:
             app.unload(on_unload)
             all_ips = get_all_ip_addresses()
             msg = f'IPs available for connection:\n{all_ips}\nNote: 0.0.0.0 is not the IP to connect. Instead use an IP above to connect and port {interface_port}'
-            msg += "\n**** Always reload the browser app page if it's already opened ****"
+            msg += "\nNote: Always reload the browser app page if it's already open."
             show_alert(None, {"type": "info", "msg": msg})
             os.environ['no_proxy'] = ' ,'.join(all_ips)
             return app
