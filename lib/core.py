a58dbf158b0f89c03d0d151b92abbe7b36bb6765
unknown <admin@radsl.net>
2026-05-16 04:39:27 -0700

    ...

--- lib/core.py
+++ lib/core.py
@@ -2587,6 +2587,8 @@ def convert_chapters2audio(session_id:str)->bool:
                 block_voice = block.get('voice') or session.get('voice')
                 for j in range(block_len):
                     if session['cancellation_requested']:
+                        msg = 'Conversion Cancelled'
+                        show_alert(session_id, {'type': 'warning', 'msg': msg})
                         return False
                     sentence = sentences[j].strip()
                     if j in valid_idx:
