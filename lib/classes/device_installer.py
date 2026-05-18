439ae142a907f9880111d8d7000e8a18af9e61c9
unknown <admin@radsl.net>
2026-05-18 07:54:45 -0700

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -1261,7 +1261,7 @@ class DeviceInstaller():
             return False
 
         def _probe_gpus()->dict:
-            script = os.path.join(os.path.dirname(__file__), 'tools', 'detect_gpus.py')
+            script = os.path.abspath('./tools/detect_gpus.py')
             try:
                 proc = subprocess.run(
                     [sys.executable, script],
