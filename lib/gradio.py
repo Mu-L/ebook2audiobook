7f76ef56034f0ae925761d0a9e8dcfd30f006fc2
unknown <admin@radsl.net>
2026-05-21 10:19:29 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -420,10 +420,10 @@ def build_interface(args:dict)->gr.Blocks:
                     background-color: #ff007f !important;
                     background-image: none !important;
                 }
-                #gr_progress textarea {
+                #gr_progress {
                     height: 100px !important;
-                    min-height: 60px !important;
-                    max-height: 60px !important;
+                    min-height: 100px !important;
+                    max-height: 100px !important;
                     resize: none;
                 }
                 #gr_session textarea::-webkit-scrollbar, #gr_progress textarea::-webkit-scrollbar {
