7e0892d8692e63245aaeeb80fe92c938d18b4f45
unknown <admin@radsl.net>
2026-06-10 18:08:11 -0700

    ...

--- lib/core.py
+++ lib/core.py
@@ -897,7 +897,7 @@ def convert2epub(session_id:str)->bool:
                 file_input = normalize_epub_zip(session_id, file_input)
                 if file_input is None:
                     return False
-                file_ext = 'epub'
+                file_ext = '.epub'
             if file_ext == '.txt':
                 with open(file_input, 'r', encoding='utf-8') as f:
                     text = f.read()
