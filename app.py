46fbdf4a9146d8e42c16324b46c24e63d93d13f6
unknown <admin@radsl.net>
2026-06-22 19:46:07 -0700

    ...

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ SML tags available:
             error = f'{FULL_DOCKER} is only an internal option for the docker itself. Use {BUILD_DOCKER} if you need to build a docker image.'
             print(error)
             sys.exit(1)
-        elif(not check_virtual_env(args['script_mode'])) or (not check_python_version(args['script_mode']):
+        elif(not check_virtual_env(args['script_mode'])) or (not check_python_version(args['script_mode'])):
             sys.exit(1)
         # Check if the port is already in use to prevent multiple launches
         if not args['headless'] and is_port_in_use(interface_port):
