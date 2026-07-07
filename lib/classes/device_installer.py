518c547a38427406650c04b7ac18f2010383ee7c
unknown <admin@radsl.net>
2026-07-06 19:19:38 -0700

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -1036,8 +1036,8 @@ class DeviceInstaller():
         packages.append(onnx_pkg)
         if self.system != systems['MACOS']:
             onnx_pkg = self.check_onnxruntime_pkg()
-            if onnx_pkg is not None:
-                packages.append(onnx_pkg)
+        if onnx_pkg is not None:
+            packages.append(onnx_pkg)
         if self.system == systems['MACOS'] and platform.machine().lower() in ('x86_64', 'amd64'):
             overrides['llvmlite'] = 'llvmlite==0.44.0'
             overrides['numba'] = 'numba==0.61.0'
